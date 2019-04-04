695aae3c18612376c731ae6e69ec2c039fcb04b0
gregoryroberts <greg.d.roberts11@gmail.com>
2019-04-04 09:36:02 -0700

    fix 936

--- inverse_design/CMOSBayerFilterFiniteDifference.py
+++ inverse_design/CMOSBayerFilterFiniteDifference.py
@@ -265,6 +265,9 @@ init_fom = compute_fom_per_focal_spot(focal_data_init)
 for fd_row in np.arange(0, device_voxels_lateral, finite_difference_downscale_factor):
 	print("Working on row = " + str(fd_row))
 	for fd_col in np.arange(0, device_voxels_lateral, finite_difference_downscale_factor):
+		fd_array_index_row = int(np.floor(fd_row / finite_difference_downscale_factor))
+		fd_array_index_col = int(np.floor(fd_col / finite_difference_downscale_factor))
+
 		cur_permittivity[fd_row, fd_col] += h
 
 		disable_all_sources()
@@ -282,7 +285,7 @@ for fd_row in np.arange(0, device_voxels_lateral, finite_difference_downscale_fa
 		compute_fd = (fom - init_fom) / h
 		print("Computed FD is " + str(compute_fd))
 
-		finite_difference_slice[fd_row, fd_col, :] = compute_fd
+		finite_difference_slice[fd_array_index_row, fd_array_index_col, :] = compute_fd
 
 		cur_permittivity[fd_row, fd_col] -= h
 
