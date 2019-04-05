6ffabb88fd3cbc060e3ec96aa3c8d556cb3a75eb
gregoryroberts <greg.d.roberts11@gmail.com>
2019-04-04 21:50:00 -0700

    fix 949

--- inverse_design/CMOSBayerFilterAdjoint.py
+++ inverse_design/CMOSBayerFilterAdjoint.py
@@ -282,9 +282,18 @@ for adj_src_idx in range(0, num_adjoint_sources):
 		adjoint_e_fields.append(
 			get_complex_monitor_data(design_efield_monitor['name'], 'E'))
 
-		# plt.imshow(np.abs(adjoint_e_fields[xy_idx][0, 0, :, :, 0]))
-		# plt.colorbar()
-		# plt.show()
+		plt.imshow(np.abs(adjoint_e_fields[xy_idx][0, 0, :, :, 0]))
+		plt.colorbar()
+		plt.show()
+
+		plt.imshow(np.abs(adjoint_e_fields[xy_idx][0, 0, :, 0, :]))
+		plt.colorbar()
+		plt.show()
+
+		plt.imshow(np.abs(adjoint_e_fields[xy_idx][0, 0, 0, :, :]))
+		plt.colorbar()
+		plt.show()
+
 
 	for pol_idx in range(0, len(polarizations)):
 		pol_name = polarizations[pol_idx]
