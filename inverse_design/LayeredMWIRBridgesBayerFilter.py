293bac974612371460b370bc45d0118c2b6c6d02
gregoryroberts <greg.d.roberts11@gmail.com>
2019-08-29 11:05:33 -0700

    test 1105

--- inverse_design/LayeredMWIRBridgesBayerFilter.py
+++ inverse_design/LayeredMWIRBridgesBayerFilter.py
@@ -305,9 +305,9 @@ class LayeredMWIRBridgesBayerFilter(device.Device):
 
 			print("To do layer " + str( layer ) + " took " + str( elapsed_patching ) + " seconds!")
 
-			for sublayer_idx in range( get_layer_idx, next_layer_idx ):
-				self.w[0][ :, :, sublayer_idx ] = patch_density
-				self.restrictions[ :, :, sublayer_idx ] = new_restrictions
+			# for sublayer_idx in range( get_layer_idx, next_layer_idx ):
+			# 	self.w[0][ :, :, sublayer_idx ] = patch_density
+			# 	self.restrictions[ :, :, sublayer_idx ] = new_restrictions
 
 		print("\n\n")	
 
