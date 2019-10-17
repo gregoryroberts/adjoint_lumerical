04fe7cd2f0074109cfe73e5b52f6e0bd85d6499f
gregoryroberts <greg.d.roberts11@gmail.com>
2019-10-17 16:39:02 -0700

    lower metal step size

--- inverse_design/CMOSMetalBayerFilterParameters.py
+++ inverse_design/CMOSMetalBayerFilterParameters.py
@@ -189,7 +189,7 @@ num_iterations_per_epoch = 15#75
 start_epoch = 3
 
 use_fixed_step_size = True
-fixed_step_size = 50 * 0.01 * 3 / 2
+fixed_step_size = 8 * 0.01 * 3 / 2
 
 epoch_start_permittivity_change_max = 0.1
 epoch_end_permittivity_change_max = 0.02
