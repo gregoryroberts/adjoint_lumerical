3fc9c4431cc2e72e84c6a17f4534e5f70312726d
gregoryroberts <greg.d.roberts11@gmail.com>
2019-10-18 10:49:23 -0700

    add binarized import capability

---
+++ inverse_design/CMOSMetalBayerFilterBinarizeImport.py
@@ -0,0 +1,325 @@
+import os
+import shutil
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
+
+from CMOSMetalBayerFilterParameters import *
+import CMOSMetalBayerFilter
+
+import lumapi
+
+import functools
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+def permittivity_to_index( permittivity ):
+	eps_real = np.real( permittivity )
+	eps_imag = np.imag( permittivity )
+
+	eps_mag = np.sqrt( eps_real**2 + eps_imag**2 )
+
+	n = np.sqrt( ( eps_mag + eps_real ) / 2. )
+	kappa = np.sqrt( ( eps_mag - eps_real ) / 2. )
+
+	return ( n + 1j * kappa )
+
+#
+# Create FDTD hook
+#
+fdtd_hook = lumapi.FDTD()
+
+#
+# Create project folder and save out the parameter file for documentation for this optimization
+#
+python_src_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
+projects_directory_location = os.path.abspath(os.path.join(os.path.dirname(__file__), '../projects/'))
+projects_directory_location += "/" + project_name
+
+if not os.path.isdir(projects_directory_location):
+	os.mkdir(projects_directory_location)
+
+fdtd_hook.newproject()
+fdtd_hook.save(projects_directory_location + "/optimization")
+
+shutil.copy2(python_src_directory + "/CMOSMetalBayerFilterParameters.py", projects_directory_location + "/ArchiveCMOSMetalBayerFilter.py")
+
+#
+# Set up the FDTD region and mesh
+#
+fdtd = fdtd_hook.addfdtd()
+fdtd['x span'] = fdtd_region_size_lateral_um * 1e-6
+fdtd['y span'] = fdtd_region_size_lateral_um * 1e-6
+fdtd['z max'] = fdtd_region_maximum_vertical_um * 1e-6
+fdtd['z min'] = fdtd_region_minimum_vertical_um * 1e-6
+fdtd['mesh type'] = 'uniform'
+fdtd['define x mesh by'] = 'number of mesh cells'
+fdtd['define y mesh by'] = 'number of mesh cells'
+fdtd['define z mesh by'] = 'number of mesh cells'
+fdtd['mesh cells x'] = fdtd_region_minimum_lateral_voxels
+fdtd['mesh cells y'] = fdtd_region_minimum_lateral_voxels
+fdtd['mesh cells z'] = fdtd_region_minimum_vertical_voxels
+fdtd['simulation time'] = fdtd_simulation_time_fs * 1e-15
+fdtd['background index'] = background_index
+
+#
+# General polarized source information
+#
+xy_phi_rotations = [0, 90]
+xy_names = ['x', 'y']
+
+
+#
+# Add a TFSF plane wave forward source at normal incidence
+#
+forward_sources = []
+
+for xy_idx in range(0, 2):
+	forward_src = fdtd_hook.addtfsf()
+	forward_src['name'] = 'forward_src_' + xy_names[xy_idx]
+	forward_src['angle phi'] = xy_phi_rotations[xy_idx]
+	forward_src['direction'] = 'Backward'
+	forward_src['x span'] = lateral_aperture_um * 1e-6
+	forward_src['y span'] = lateral_aperture_um * 1e-6
+	forward_src['z max'] = src_maximum_vertical_um * 1e-6
+	forward_src['z min'] = src_minimum_vertical_um * 1e-6
+	forward_src['wavelength start'] = lambda_min_um * 1e-6
+	forward_src['wavelength stop'] = lambda_max_um * 1e-6
+
+	forward_sources.append(forward_src)
+
+#
+# Place dipole adjoint sources at the focal plane that can ring in both
+# x-axis and y-axis
+#
+adjoint_sources = []
+
+for adj_src_idx in range(0, num_adjoint_sources):
+	adjoint_sources.append([])
+	for xy_idx in range(0, 2):
+		adj_src = fdtd_hook.adddipole()
+		adj_src['name'] = 'adj_src_' + str(adj_src_idx) + xy_names[xy_idx]
+		adj_src['x'] = adjoint_x_positions_um[adj_src_idx] * 1e-6
+		adj_src['y'] = adjoint_y_positions_um[adj_src_idx] * 1e-6
+		adj_src['z'] = adjoint_vertical_um * 1e-6
+		adj_src['theta'] = 90
+		adj_src['phi'] = xy_phi_rotations[xy_idx]
+		adj_src['wavelength start'] = lambda_min_um * 1e-6
+		adj_src['wavelength stop'] = lambda_max_um * 1e-6
+
+		adjoint_sources[adj_src_idx].append(adj_src)
+
+#
+# Set up the volumetric electric field monitor inside the design region.  We will need this compute
+# the adjoint gradient
+#
+design_efield_monitor = fdtd_hook.addprofile()
+design_efield_monitor['name'] = 'design_efield_monitor'
+design_efield_monitor['monitor type'] = '3D'
+design_efield_monitor['x span'] = device_size_lateral_um * 1e-6
+design_efield_monitor['y span'] = device_size_lateral_um * 1e-6
+design_efield_monitor['z min'] = designable_device_vertical_minimum_um * 1e-6
+design_efield_monitor['z max'] = designable_device_vertical_maximum_um * 1e-6
+design_efield_monitor['override global monitor settings'] = 1
+design_efield_monitor['use linear wavelength spacing'] = 1
+design_efield_monitor['use source limits'] = 1
+design_efield_monitor['frequency points'] = num_design_frequency_points
+design_efield_monitor['output Hx'] = 0
+design_efield_monitor['output Hy'] = 0
+design_efield_monitor['output Hz'] = 0
+
+#
+# Set up adjoint point monitors to get electric field strength at focus spots.  This will allow us to
+# compute the figure of merit as well as weight the adjoint simulations properly in calculation of the
+# gradient.
+#
+focal_monitors = []
+
+for adj_src in range(0, num_adjoint_sources):
+	focal_monitor = fdtd_hook.addpower()
+	focal_monitor['name'] = 'focal_monitor_' + str(adj_src)
+	focal_monitor['monitor type'] = 'point'
+	focal_monitor['x'] = adjoint_x_positions_um[adj_src] * 1e-6
+	focal_monitor['y'] = adjoint_y_positions_um[adj_src] * 1e-6
+	focal_monitor['z'] = adjoint_vertical_um * 1e-6
+	focal_monitor['override global monitor settings'] = 1
+	focal_monitor['use linear wavelength spacing'] = 1
+	focal_monitor['use source limits'] = 1
+	focal_monitor['frequency points'] = num_design_frequency_points
+
+	focal_monitors.append(focal_monitor)
+
+
+#
+# Build the dielectric stack on top of the metal
+#
+num_dielectric_layers = len( top_dielectric_layer_thickness_um )
+for dielectric_layer_idx in range( 0, num_dielectric_layers ):
+	dielectric_layer = fdtd_hook.addrect()
+	dielectric_layer['name'] = 'dielectric_layer_' + str( dielectric_layer_idx )
+	dielectric_layer['x span'] = fdtd_region_size_lateral_um * 1e-6
+	dielectric_layer['y span'] = fdtd_region_size_lateral_um * 1e-6
+
+	dielectric_layer_z_min_um = dielectric_stack_start_um + np.sum( top_dielectric_layer_thickness_um[ 0 : dielectric_layer_idx ] )
+	dielectric_layer_z_max_um = dielectric_layer_z_min_um + top_dielectric_layer_thickness_um[ dielectric_layer_idx ]
+
+	dielectric_layer['z min'] = dielectric_layer_z_min_um * 1e-6
+	dielectric_layer['z max'] = dielectric_layer_z_max_um * 1e-6
+
+	dielectric_layer['index'] = top_dielectric_layer_refractice_index[ dielectric_layer_idx ]
+
+
+#
+# Make bottom layer reflective for a reflective device.  We will do this with an nk2 material that we import the maximum
+# real and imaginary permittivity parts in for that we are using for the design.  Thus, it will reflect and account for
+# metallic loss
+#
+metal_reflector_import = fdtd_hook.addimport()
+metal_reflector_import['name'] = 'bottom_metal_reflector'
+metal_reflector_import['x span'] = fdtd_region_size_lateral_um * 1e-6
+metal_reflector_import['y span'] = fdtd_region_size_lateral_um * 1e-6
+metal_reflector_import['z min'] = bottom_metal_reflector_start_um * 1e-6
+metal_reflector_import['z max'] = bottom_metal_reflector_end_um * 1e-6
+
+metal_reflector_permittivity = (
+		( max_real_permittivity + 1j * max_imag_permittivity ) *
+		np.ones( ( fdtd_region_size_lateral_voxels, fdtd_region_size_lateral_voxels, bottom_metal_reflector_size_vertical_voxels ) )
+	)
+metal_reflector_index = permittivity_to_index( metal_reflector_permittivity )
+
+metal_reflector_region_x = 1e-6 * np.linspace(-0.5 * fdtd_region_size_lateral_um, 0.5 * fdtd_region_size_lateral_um, fdtd_region_size_lateral_voxels)
+metal_reflector_region_y = 1e-6 * np.linspace(-0.5 * fdtd_region_size_lateral_um, 0.5 * fdtd_region_size_lateral_um, fdtd_region_size_lateral_voxels)
+metal_reflector_region_z = 1e-6 * np.linspace(bottom_metal_reflector_start_um, bottom_metal_reflector_end_um, bottom_metal_reflector_size_vertical_voxels)
+
+fdtd_hook.select('bottom_metal_reflector')
+fdtd_hook.importnk2(metal_reflector_index, metal_reflector_region_x, metal_reflector_region_y, metal_reflector_region_z)
+
+
+#
+# Add device region and create device permittivity
+#
+design_import = fdtd_hook.addimport()
+design_import['name'] = 'design_import'
+design_import['x span'] = device_size_lateral_um * 1e-6
+design_import['y span'] = device_size_lateral_um * 1e-6
+design_import['z min'] = designable_device_vertical_minimum_um * 1e-6
+design_import['z max'] = designable_device_vertical_maximum_um * 1e-6
+
+min_device_permittivity = min_real_permittivity + 1j * min_imag_permittivity
+max_device_permittivity = max_real_permittivity + 1j * max_imag_permittivity
+
+bayer_filter_size_voxels = np.array([device_voxels_lateral, device_voxels_lateral, designable_device_voxels_vertical])
+bayer_filter = CMOSMetalBayerFilter.CMOSMetalBayerFilter(
+	bayer_filter_size_voxels, [min_device_permittivity, max_device_permittivity], init_permittivity_0_1_scale, num_vertical_layers)
+
+bayer_filter.set_design_variable( np.load( projects_directory_location + "/cur_design_variable.npy" ) )
+
+bayer_filter_region_x = 1e-6 * np.linspace(-0.5 * device_size_lateral_um, 0.5 * device_size_lateral_um, device_voxels_lateral)
+bayer_filter_region_y = 1e-6 * np.linspace(-0.5 * device_size_lateral_um, 0.5 * device_size_lateral_um, device_voxels_lateral)
+bayer_filter_region_z = 1e-6 * np.linspace(designable_device_vertical_minimum_um, designable_device_vertical_maximum_um, designable_device_voxels_vertical)
+
+
+#
+# Add blocks of dielectric on the side of the designable region because we will be leaving those as blank, unpatterned dielectric material
+# Would it be better to have this be a part of the bayer filter material inmport and jus tnot modify it (i.e. - mask out any changes to it).  I'm
+# thinking of how the mesh behaves here between these interacting objects.  For now, we will add the blocks around the side because this will make
+# it simpler at first and then we can move towards making a more sophisticated device class or subclass.
+# Further, in reality, this may be a stack of material in general.  However, it will be mostly the low-K dielctric background material so we will assume
+# this is not a stratified stack and is actaully just a single piece of low index material background.
+#
+extra_lateral_space_per_side_um = 0.5 * ( fdtd_region_size_lateral_um - device_size_lateral_um )
+extra_lateral_space_offset_um = 0.5 * ( device_size_lateral_um + extra_lateral_space_per_side_um )
+
+def side_to_string( side_number ):
+	side_integer = int( side_number )
+	if side_integer < 0:
+		return ( "n" + str( np.abs( side_integer ) ) )
+	else:
+		return str( side_integer )
+
+device_background_side_x = [ -1, 1, 0, 0 ]
+device_background_side_y = [ 0, 0, -1, 1 ]
+
+for device_background_side_idx in range( 0, 4 ):
+	side_x = device_background_side_x[ device_background_side_idx ]
+	side_y = device_background_side_y[ device_background_side_idx ]
+
+	side_block = fdtd_hook.addrect()
+
+	side_block['name'] = 'device_background_' + side_to_string( side_x ) + "_" + side_to_string( side_y )
+	side_block['z min'] = designable_device_vertical_minimum_um * 1e-6
+	side_block['z max'] = designable_device_vertical_maximum_um * 1e-6
+	side_block['x'] = side_x * extra_lateral_space_offset_um * 1e-6
+	side_block['x span'] = (
+		np.abs( side_x ) * extra_lateral_space_per_side_um +
+		( 1 - np.abs( side_x ) ) * fdtd_region_size_lateral_um ) * 1e-6
+	side_block['y'] = side_y * extra_lateral_space_offset_um * 1e-6
+	side_block['y span'] = (
+		np.abs( side_y ) * extra_lateral_space_per_side_um +
+		( 1 - np.abs( side_y ) ) * fdtd_region_size_lateral_um ) * 1e-6
+
+	side_block['index'] = device_background_index
+
+
+#
+# Disable all sources in the simulation, so that we can selectively turn single sources on at a time
+#
+def disable_all_sources():
+	fdtd_hook.switchtolayout()
+
+	for xy_idx in range(0, 2):
+		(forward_sources[xy_idx]).enabled = 0
+
+	for adj_src_idx in range(0, num_adjoint_sources):
+		for xy_idx in range(0, 2):
+			(adjoint_sources[adj_src_idx][xy_idx]).enabled = 0
+
+#
+# Consolidate the data transfer functionality for getting data from Lumerical FDTD process to
+# python process.  This is much faster than going through Lumerical's interop library
+#
+def get_monitor_data(monitor_name, monitor_field):
+	lumerical_data_name = "monitor_data_" + monitor_name + "_" + monitor_field
+	extracted_data_name = lumerical_data_name + "_data"
+	data_transfer_filename = projects_directory_location + "/data_transfer_" + monitor_name + "_" + monitor_field
+
+	command_read_monitor = lumerical_data_name + " = getresult(\'" + monitor_name + "\', \'" + monitor_field + "\');"
+	command_extract_data = extracted_data_name + " = " + lumerical_data_name + "." + monitor_field + ";"
+	command_save_data_to_file = "matlabsave(\'" + data_transfer_filename + "\', " + extracted_data_name + ");"
+
+	lumapi.evalScript(fdtd_hook.handle, command_read_monitor)
+	lumapi.evalScript(fdtd_hook.handle, command_extract_data)
+
+	# start_time = time.time()
+
+	lumapi.evalScript(fdtd_hook.handle, command_save_data_to_file)
+	monitor_data = {}
+	load_file = h5py.File(data_transfer_filename + ".mat")
+
+	monitor_data = np.array(load_file[extracted_data_name])
+
+	# end_time = time.time()
+
+	# print("\nIt took " + str(end_time - start_time) + " seconds to transfer the monitor data\n")
+
+	return monitor_data
+
+def get_complex_monitor_data(monitor_name, monitor_field):
+	data = get_monitor_data(monitor_name, monitor_field)
+	return (data['real'] + np.complex(0, 1) * data['imag'])
+
+
+fdtd_hook.switchtolayout()
+design_variable = bayer_filter.get_design_variable()
+bin_design_variable = np.ones( design_variable.shape ) * np.greater( design_variable, 0.5 )
+bayer_filter.set_design_variable( bin_design_variable )
+cur_permittivity = bayer_filter.get_permittivity()
+
+cur_index = permittivity_to_index( cur_permittivity )
+fdtd_hook.select("design_import")
+fdtd_hook.importnk2(cur_index, bayer_filter_region_x, bayer_filter_region_y, bayer_filter_region_z)
+
+
+
