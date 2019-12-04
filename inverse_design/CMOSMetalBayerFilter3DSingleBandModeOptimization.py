4cad800709ea52379cbb01937515359d22514c8c
Greg Roberts <greg.d.roberts11@gmail.com>
2019-12-04 09:19:56 -0800

    test overlap

--- inverse_design/CMOSMetalBayerFilter3DSingleBandModeOptimization.py
+++ inverse_design/CMOSMetalBayerFilter3DSingleBandModeOptimization.py
@@ -224,7 +224,7 @@ mode_h_fields = {}
 # == 377.1
 mu_nought_c = ( 1.257 * 1e-6 ) * ( 3.0 * 1e8 )
 
-monitor_lateral_voxels = 1 + int( mode_reflection_monitor[ 'x span' ] / mesh_spacing_um )
+monitor_lateral_voxels = 2 + int( 1e6 * mode_reflection_monitor[ 'x span' ] / mesh_spacing_um )
 # Organize these as freq, pol, z, y, x
 
 mode_e_field_xpol = np.zeros( ( num_design_frequency_points, 3, 1, monitor_lateral_voxels, monitor_lateral_voxels ), dtype=np.complex )
@@ -239,6 +239,13 @@ mode_h_field_ypol = np.zeros( ( num_design_frequency_points, 3, 1, monitor_later
 
 mode_e_field_ypol[ :, 1, :, :, : ] = 1
 mode_h_field_ypol[ :, 0, :, :, : ] = -( 1. / mu_nought_c )
+
+mode_e_fields[ 'x' ] = mode_e_field_xpol
+mode_h_fields[ 'x' ] = mode_h_field_xpol
+
+mode_e_fields[ 'y' ] = mode_e_field_ypol
+mode_h_fields[ 'y' ] = mode_h_field_ypol
+
 
 phase_corrections_reflection = np.zeros( num_design_frequency_points, dtype=np.complex )
 
