6d7915607c7e8aade1f89e795235ec2c13fe89e7
Greg Roberts <greg.d.roberts11@gmail.com>
2019-12-19 16:44:57 -0800

    neural generation

--- inverse_design/GenerateColorSplitterDataParameters.py
+++ inverse_design/GenerateColorSplitterDataParameters.py
@@ -30,31 +30,29 @@ lambda_minus_bounds_um = [ 0.05, 0.2 ]
 #
 # Device Geometry
 #
-aperture_size_bounds_lambda_plus_units = [ 2, 8 ]
-device_depth_bounds_lambda_plus_units = [ 2, 8 ]
+aperture_size_lambda_plus_units = 4
+device_depth_bounds_lambda_plus_units = [ 1, 4 ]
 # we will measure focal length from the end of the device
 focal_length_bounds_lambda_plus_units = [ 1, 6 ]
+# we will use rough formula of:
+# NA ~ n * W / ( 2 * f ), where n = 1.0 (imaging in air), W is the aperture size, f is the focal distance (which we will take to be from the end of the device)
+# so in order to pick focal length, we compute it via:
+# f = NA * W / 2
+numerical_aperture_bounds = [ 0.25, 1.5 ]
+
+aperture_size_um = lambda_plus_um * aperture_size_lambda_plus_units
 
 #
 # Simulation Region
 #
-lateral_gap_lambda_plus = 3
-vertical_gap_lambda_plus = 3
+lateral_gap_lambda_plus = 2
+vertical_gap_lambda_plus = 2
 
 lateral_gap_um = lambda_plus_um * lateral_gap_lambda_plus
 vertical_gap_um = lambda_plus_um * vertical_gap_lambda_plus
 
-mesh_spacing_lambda_plus = 1. / 15.
+mesh_spacing_lambda_plus = 1. / 12.
 mesh_spacing_um = lambda_plus_um * mesh_spacing_lambda_plus
-
-#
-# Device
-#
-mesh_spacing_um = 0.025
-
-c_eps_nought = ( 3.0 * 1e8 ) * ( 8.854 * 1e-12 )
-
-
 
 fdtd_simulation_time_fs = 10000
 
@@ -66,7 +64,11 @@ num_adjoint_sources = 2
 #
 # Optimization
 #
-num_iterations = 0
+min_iterations = 40
+max_iterations = 50
+gradient_norm_dropoff = 0.05
 
-start_design_change_max = 0.05
-end_design_change_max = 0.005
+num_collect_step_size_iterations = 5
+
+start_design_change_max = 0.03
+step_design_change_max = 0.03
