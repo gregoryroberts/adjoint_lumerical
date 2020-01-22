433da3ca06fc59a941fd62faea9bfe9e21b3bb59
Greg Roberts <greg.d.roberts11@gmail.com>
2020-01-22 14:17:39 -0800

    add amorph si and su8 design

---
+++ inverse_design/LayeredLithographyIRBayerFilter.py
@@ -0,0 +1,114 @@
+import device as device
+import layering as layering
+import scale as scale
+import sigmoid as sigmoid
+import square_blur as square_blur
+
+import numpy as np
+
+from LayeredLithographyIRParameters import *
+
+class LayeredLithographyIRBayerFilter(device.Device):
+
+	def __init__(self, size, permittivity_bounds, init_permittivity, num_z_layers, spacer_height_voxels):
+		super(LayeredLithographyIRBayerFilter, self).__init__(size, permittivity_bounds, init_permittivity)
+
+		self.num_z_layers = num_z_layers
+		self.flip_threshold = 0.5
+		self.minimum_design_value = 0
+		self.maximum_design_value = 1
+        self.spacer_height_voxels = spacer_height_voxels
+		self.init_filters_and_variables()
+
+		self.update_permittivity()
+
+
+	#
+	# Override the update_permittivity function so we can handle layer-dependent collapsing along either x- or y-dimensions
+	#
+	def update_permittivity(self):
+		var0 = self.w[0]
+
+		var1 = self.sigmoid_0.forward(var0)
+		self.w[1] = var1
+
+		var2 = self.layering_z_1.forward(var1)
+		self.w[2] = var2
+
+		var3 = self.max_blur_xy_2.forward(var2)
+		self.w[3] = var3
+
+		var4 = self.sigmoid_3.forward(var3)
+		self.w[3] = var4
+
+		var5 = self.scale_4.forward(var4)
+		self.w[5] = var5
+
+
+	#
+	# Need to also override the backpropagation function
+	#
+	def backpropagate(self, gradient):
+		gradient = self.scale_4.chain_rule(gradient, self.w[5], self.w[4])
+		gradient = self.sigmoid_3.chain_rule(gradient, self.w[4], self.w[3])
+		gradient = self.max_blur_xy_2.chain_rule(gradient, self.w[3], self.w[2])
+		gradient = self.layering_z_1.chain_rule(gradient, self.w[2], self.w[1])
+		gradient = self.sigmoid_0.chain_rule(gradient, self.w[1], self.w[0])
+
+		return gradient
+
+	def update_filters(self, epoch):
+		self.sigmoid_beta = 0.25 * (2**epoch)
+
+		self.sigmoid_0 = sigmoid.Sigmoid(self.sigmoid_beta, self.sigmoid_eta)
+		self.sigmoid_2 = sigmoid.Sigmoid(self.sigmoid_beta, self.sigmoid_eta)
+		self.filters = [self.sigmoid_0, self.layering_z_1, self.max_blur_xy_2, self.sigmoid_3, self.scale_4]
+
+	def init_filters_and_variables(self):
+		self.num_filters = 5
+		self.num_variables = 1 + self.num_filters
+
+		# Start the sigmoids at weak strengths
+		self.sigmoid_beta = 0.0625
+		self.sigmoid_eta = 0.5
+		self.sigmoid_0 = sigmoid.Sigmoid(self.sigmoid_beta, self.sigmoid_eta)
+		self.sigmoid_3 = sigmoid.Sigmoid(self.sigmoid_beta, self.sigmoid_eta)
+
+		x_dimension_idx = 0
+		y_dimension_idx = 1
+		z_dimension_idx = 2
+
+		z_voxel_layers = self.size[2]
+		self.layering_z_1 = layering.Layering(z_dimension_idx, self.num_z_layers, [0, 1], self.spacer_height_voxels, 0.0)
+
+		alpha = 8
+		self.blur_half_width = blur_half_width_voxels
+		#
+		# This notation is slightly confusing, but it is meant to be the
+		# direction you blur when you are on the layer corresponding to x-
+		# or y-layering.  So, if you are layering in x, then you blur in y
+		# and vice versa.
+		#
+		self.max_blur_xy_2 = square_blur.SquareBlur(
+			alpha,
+			[self.blur_half_width, self.blur_half_width, 0])
+
+		scale_min = self.permittivity_bounds[0]
+		scale_max = self.permittivity_bounds[1]
+		self.scale_4 = scale.Scale([scale_min, scale_max])
+
+		# Initialize the filter chain
+		self.filters = [self.sigmoid_0, self.layering_z_1, self.max_blur_xy_2, self.sigmoid_3, self.scale_4]
+
+		self.init_variables()
+
+	# In the step function, we should update the permittivity with update_permittivity
+	def step(self, gradient, step_size):
+        # We are not rebinning the gradient here, but we can do this again if we want to guarantee feature
+        # and hole size minimums
+		self.w[0] = self.proposed_design_step(gradient, step_size)
+		# Update the variable stack including getting the permittivity at the w[-1] position
+		self.update_permittivity()
+
+	def convert_to_binary_map(self, variable):
+		return np.greater(variable, self.mid_permittivity)
---
+++ inverse_design/LayeredLithographyIRParameters.py
@@ -0,0 +1,136 @@
+#
+# Parameter file for the Bayer Filter Layered Lithography optimization
+#
+
+import numpy as np
+
+#
+# Files
+#
+project_name = 'layered_infrared_3layers_2x2x2um'
+
+#
+# Optical
+#
+# averaging between the part where we dip into the infrared/red part of the spectrum
+# and where we are in the telecom ranges
+#
+index_silicon = 0.5 * ( 3.47 + 3.86 )
+index_su8 = 1.575
+background_index = index_silicon
+min_device_index = index_su8
+max_device_index = index_silicon
+
+min_device_permittivity = min_device_index**2
+max_device_permittivity = max_device_index**2
+
+init_permittivity_0_1_scale = 0.25
+
+focal_length_um = 1.5
+focal_plane_center_lateral_um = 0
+focal_plane_center_vertical_um = -focal_length_um
+
+#
+# Device
+#
+mesh_spacing_um = 0.032
+
+#
+# Pesky size to get the number of voxels to be a multiple of 3
+#
+num_vertical_layers = 3
+
+device_size_lateral_um = 4
+device_size_verical_um = num_vertical_layers * ( 4.0 / num_vertical_layers )
+amorphous_silicon_height_per_layer_um = 0.8
+spacer_size_um = ( device_size_verical_um / num_vertical_layers ) - amorphous_silicon_height_per_layer_um
+
+device_voxels_lateral = 1 + int(device_size_lateral_um / mesh_spacing_um)
+device_voxels_vertical = 1 + int(device_size_verical_um / mesh_spacing_um)
+spacer_size_voxels = 1 + int(spacer_size_um / mesh_spacing_um)
+
+device_vertical_maximum_um = device_size_verical_um
+device_vertical_minimum_um = 0
+
+#
+# Spectral
+#
+lambda_min_um = 0.9
+lambda_max_um = 1.55
+
+num_bands = 3
+num_points_per_band = 10
+num_design_frequency_points = num_bands * num_points_per_band
+
+lambda_values_um = np.linspace(lambda_min_um, lambda_max_um, num_design_frequency_points)
+max_intensity_by_wavelength = (device_size_lateral_um**2)**2 / (focal_length_um**2 * lambda_values_um**2)
+
+#
+# Fabrication Constraints
+#
+min_feature_size_um = 3 * mesh_spacing_um
+min_feature_size_voxels = min_feature_size_um / mesh_spacing_um
+blur_half_width_voxels = int( np.ceil( (min_feature_size_voxels - 1) / 2. ) )
+
+
+#
+# FDTD
+#
+vertical_gap_size_um = 1.0
+lateral_gap_size_um = 1.0
+
+fdtd_region_size_vertical_um = 2 * vertical_gap_size_um + device_size_verical_um + focal_length_um
+fdtd_region_size_lateral_um = 2 * lateral_gap_size_um + device_size_lateral_um
+fdtd_region_maximum_vertical_um = device_size_verical_um + vertical_gap_size_um
+fdtd_region_minimum_vertical_um = -focal_length_um - vertical_gap_size_um
+
+fdtd_region_minimum_vertical_voxels = int( np.ceil(fdtd_region_size_vertical_um / mesh_spacing_um) )
+fdtd_region_minimum_lateral_voxels = int( np.ceil(fdtd_region_size_lateral_um / mesh_spacing_um) )
+
+fdtd_simulation_time_fs = 700
+
+#
+# Forward Source
+#
+lateral_aperture_um = device_size_lateral_um + 0.2
+src_maximum_vertical_um = device_size_verical_um + 0.5 * vertical_gap_size_um
+src_minimum_vertical_um = -focal_length_um - 0.5 * vertical_gap_size_um
+
+#
+# Spectral and polarization selectivity information
+#
+polarizations_focal_plane_map = [ ['x', 'y'], ['x'], ['x', 'y'], ['y'] ]
+weight_focal_plane_map = [ 1.0, 0.5, 1.0, 0.5 ]
+polarization_name_to_idx = { 'x':0, 'y':1, 'z':2 }
+# We are assuming that the data is organized in order of increasing wavelength (i.e. - blue first, red last)
+spectral_focal_plane_map = [
+	[0, num_points_per_band],
+	[num_points_per_band, 2 * num_points_per_band],
+	[2 * num_points_per_band, 3 * num_points_per_band],
+	[num_points_per_band, 2 * num_points_per_band]
+]
+
+#
+# Adjoint sources
+#
+adjoint_vertical_um = -focal_length_um
+num_focal_spots = 4
+num_adjoint_sources = num_focal_spots
+adjoint_x_positions_um = [device_size_lateral_um / 4., -device_size_lateral_um / 4., -device_size_lateral_um / 4., device_size_lateral_um / 4.]
+adjoint_y_positions_um = [device_size_lateral_um / 4., device_size_lateral_um / 4., -device_size_lateral_um / 4., -device_size_lateral_um / 4.]
+
+#
+# Optimization
+#
+start_epoch = 0
+num_epochs = 8
+num_iterations_per_epoch = 25
+
+epoch_start_permittivity_change_max = 0.1
+epoch_end_permittivity_change_max = 0.02
+epoch_range_permittivity_change_max = epoch_start_permittivity_change_max - epoch_end_permittivity_change_max
+
+epoch_start_permittivity_change_min = 0.05
+epoch_end_permittivity_change_min = 0
+epoch_range_permittivity_change_min = epoch_start_permittivity_change_min - epoch_end_permittivity_change_min
+
