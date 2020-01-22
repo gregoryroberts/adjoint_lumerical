31174b3833f7580d82bbd03bd19d648478cb5922
Greg Roberts <greg.d.roberts11@gmail.com>
2020-01-22 14:22:51 -0800

    add amorph si and su8 design

--- inverse_design/layering.py
+++ inverse_design/layering.py
@@ -9,10 +9,12 @@ import numpy as np
 #
 class Layering(filter.Filter):
 
-	def __init__(self, dim, num_layers, variable_bounds=[0, 1]):
+	def __init__(self, dim, num_layers, variable_bounds=[0, 1], spacer_height_voxels=0, spacer_voxels_value=0):
 		super(Layering, self).__init__(variable_bounds)
 
 		self.dim = dim
+		self.spacer_height_voxels = spacer_height_voxels
+		self.spacer_voxels_value = spacer_voxels_value
 		self.num_layers = num_layers
 
 	def get_layer_idxs(self, variable_shape):
@@ -49,7 +51,7 @@ class Layering(filter.Filter):
 
 		return output
 
-	def layer_averaging(self, variable):
+	def layer_averaging(self, variable, spacer_value):
 		variable_shape = variable.shape
 		num_voxels_total = variable_shape[self.dim]
 
@@ -62,23 +64,32 @@ class Layering(filter.Filter):
 			layer_start = layer_idx * num_voxels_per_layer
 
 			idx_bunch = [slice(None)] * variable.ndim
-			idx_bunch[self.dim] = np.arange(layer_start, layer_start + num_voxels_per_layer)
+			idx_bunch[self.dim] = np.arange(layer_start, layer_start + num_voxels_per_layer - self.spacer_height_voxels)
+
+			idx_bunch_spacer = [slice(None)] * variable.ndim
+			idx_bunch_spacer[self.dim] = np.arange(layer_start + num_voxels_per_layer - self.spacer_height_voxels, layer_start + num_voxels_per_layer)
 
 			variable_out[tuple(idx_bunch)] = self.do_layering(variable[tuple(idx_bunch)], self.dim, variable_out[tuple(idx_bunch)])
+			variable_out[tuple(idx_bunch_spacer)] = spacer_value
+
 
 		last_layer_start = (self.num_layers - 1) * num_voxels_per_layer
 		idx_bunch = [slice(None)] * variable.ndim
-		idx_bunch[self.dim] = np.arange(last_layer_start, num_voxels_total)
+		idx_bunch[self.dim] = np.arange(last_layer_start, num_voxels_total - self.spacer_height_voxels)
+
+		idx_bunch_spacer = [slice(None)] * variable.ndim
+		idx_bunch_spacer[self.dim] = np.arange(num_voxels_total - self.spacer_height_voxels, num_voxels_total)
 
 		variable_out[tuple(idx_bunch)] = self.do_layering(variable[tuple(idx_bunch)], self.dim, variable_out[tuple(idx_bunch)])
+		variable_out[tuple(idx_bunch_spacer)] = spacer_value
 
 		return variable_out
 
 	def forward(self, variable_in):
-		return self.layer_averaging(variable_in)
+		return self.layer_averaging(variable_in, self.spacer_voxels_value)
 
 	def chain_rule(self, derivative_out, variable_out_, variable_in_):
-		return self.layer_averaging(derivative_out)
+		return self.layer_averaging(derivative_out, 0)
 
 	def fabricate(self, variable_in):
 		return self.forward(variable_in)
