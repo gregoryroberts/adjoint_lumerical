fixes and move gradient

inverse_design/MultiStageForceBinarization2DMultiDeviceOptimization.py
@@ -489,43 +489,40 @@ for optimization_state_idx in range( init_optimization_state, num_optimization_s
 									axis=0)
 
 
-				xy_polarized_gradients_by_pol[ pol_idx ] = polarized_gradient
-				xy_polarized_gradients_by_pol_lsf[ pol_idx ] = polarized_gradient
+					xy_polarized_gradients_by_pol[ pol_idx ] = polarized_gradient
+					xy_polarized_gradients_by_pol_lsf[ pol_idx ] = polarized_gradient
 
-			weight_grad_by_pol = ( 2. / num_polarizations ) - figure_of_merit_by_pol**2 / np.sum( figure_of_merit_by_pol**2 )
-			weight_grad_by_pol = np.maximum( weight_grad_by_pol, 0 )
-			weight_grad_by_pol /= np.sum( weight_grad_by_pol )
+				weight_grad_by_pol = ( 2. / num_polarizations ) - figure_of_merit_by_pol**2 / np.sum( figure_of_merit_by_pol**2 )
+				weight_grad_by_pol = np.maximum( weight_grad_by_pol, 0 )
+				weight_grad_by_pol /= np.sum( weight_grad_by_pol )
 
-			print(figure_of_merit_by_pol)
-			print(weight_grad_by_pol)
-			print()
+				for pol_idx in range( 0, num_polarizations ):
+					xy_polarized_gradients += weight_grad_by_pol[ pol_idx ] * xy_polarized_gradients_by_pol[ pol_idx ]
+					xy_polarized_gradients_lsf += weight_grad_by_pol[ pol_idx ] * xy_polarized_gradients_by_pol_lsf[ pol_idx ]
 
-			for pol_idx in range( 0, num_polarizations ):
-				xy_polarized_gradients += weight_grad_by_pol[ pol_idx ] * xy_polarized_gradients_by_pol[ pol_idx ]
-				xy_polarized_gradients_lsf += weight_grad_by_pol[ pol_idx ] * xy_polarized_gradients_by_pol_lsf[ pol_idx ]
+				#
+				# Step 4: Step the design variable.
+				#
+				device_gradient_real = 2 * np.real( xy_polarized_gradients )
+				device_gradient_imag = 2 * np.imag( xy_polarized_gradients )
+				# Because of how the data transfer happens between Lumerical and here, the axes are ordered [z, y, x] when we expect them to be
+				# [x, y, z].  For this reason, we swap the 0th and 2nd axes to get them into the expected ordering.
+				device_gradient_real = np.swapaxes(device_gradient_real, 0, 2)
+				device_gradient_imag = np.swapaxes(device_gradient_imag, 0, 2)
 
-			#
-			# Step 4: Step the design variable.
-			#
-			device_gradient_real = 2 * np.real( xy_polarized_gradients )
-			device_gradient_imag = 2 * np.imag( xy_polarized_gradients )
-			# Because of how the data transfer happens between Lumerical and here, the axes are ordered [z, y, x] when we expect them to be
-			# [x, y, z].  For this reason, we swap the 0th and 2nd axes to get them into the expected ordering.
-			device_gradient_real = np.swapaxes(device_gradient_real, 0, 2)
-			device_gradient_imag = np.swapaxes(device_gradient_imag, 0, 2)
+				device_gradient_real_lsf = 2 * np.real( xy_polarized_gradients_lsf )
+				device_gradient_imag_lsf = 2 * np.imag( xy_polarized_gradients_lsf )
+				# Because of how the data transfer happens between Lumerical and here, the axes are ordered [z, y, x] when we expect them to be
+				# [x, y, z].  For this reason, we swap the 0th and 2nd axes to get them into the expected ordering.
+				device_gradient_real_lsf = np.swapaxes(device_gradient_real_lsf, 0, 2)
+				device_gradient_imag_lsf = np.swapaxes(device_gradient_imag_lsf, 0, 2)
 
-			device_gradient_real_lsf = 2 * np.real( xy_polarized_gradients_lsf )
-			device_gradient_imag_lsf = 2 * np.imag( xy_polarized_gradients_lsf )
-			# Because of how the data transfer happens between Lumerical and here, the axes are ordered [z, y, x] when we expect them to be
-			# [x, y, z].  For this reason, we swap the 0th and 2nd axes to get them into the expected ordering.
-			device_gradient_real_lsf = np.swapaxes(device_gradient_real_lsf, 0, 2)
-			device_gradient_imag_lsf = np.swapaxes(device_gradient_imag_lsf, 0, 2)
+				gradients_real[ device, : ] = device_gradient_real
+				gradients_imag[ device, : ] = device_gradient_imag
+				gradients_real_lsf[ device, : ] = device_gradient_real_lsf
+				gradients_imag_lsf[ device, : ] = device_gradient_imag_lsf
 
-			gradients_real[ device, : ] = device_gradient_real
-			gradients_imag[ device, : ] = device_gradient_imag
-			gradients_real_lsf[ device, : ] = device_gradient_real_lsf
-			gradients_imag_lsf[ device, : ] = device_gradient_imag_lsf
-
+			print( 'Figure of merit by device = ' + str( figure_of_merit_by_device ) )
 			my_optimization_state.submit_figure_of_merit( figure_of_merit_by_device, iteration, epoch )
 			my_optimization_state.update( -gradients_real, -gradients_imag, -gradients_real_lsf, -gradients_imag_lsf, epoch, iteration )
 

inverse_design/OptimizationLayersSpacersGlobalBinarization2DMultiDevice.py
@@ -248,7 +248,7 @@ class OptimizationLayersSpacersGlobalBinarization2DMultiDevice( OptimizationStat
 
 			if self.layer_designability[ layer_idx ]:
 				backprop = combined_density_layer_gradients[ bayer_idx ]
-				get_design_variable = self.bayer_filters[ bayer_idx ][ self.device_idx_to_binarize ].w[ 0 ]
+				get_design_variable = self.bayer_filters[ bayer_idx ][ self.device_idx_to_binarize ].get_design_variable()
 
 				assert len( get_design_variable.shape ) == 2, "Not 2-dimensional design space!"
 				collect_fom_gradients.append( backprop[ :, 0 ] )
@@ -284,7 +284,7 @@ class OptimizationLayersSpacersGlobalBinarization2DMultiDevice( OptimizationStat
 		# For now, ignore the beta specified because we are going to ensure
 		# that we get the requested binarization improvement
 		beta_low = 0
-		beta_high = self.max_binarize_movement#2 * self.max_binarize_movement
+		beta_high = self.max_binarize_movement
 		projected_binarization_increase = 0
 
 		c = flatten_fom_gradients
@@ -354,9 +354,17 @@ class OptimizationLayersSpacersGlobalBinarization2DMultiDevice( OptimizationStat
 
 		ending_binarization = compute_binarization( proposed_design_variable )
 
-		# import matplotlib.pyplot as plt
-		# plt.plot( cur_x, color='b', linewidth=2 )
-		# plt.show()
+		expected_binarization_change = np.dot( x_star, b )
+		actual_binarization_change = ending_binarization - starting_binarization
+
+		if expected_binarization_change < 0:
+			np.save( 'fom_gradients_debug.npy', c )
+			np.save( 'binarization_gradients_debug.npy', b )
+			np.save( 'upper_bounds_debug.npy', upper_bounds )
+			np.save( 'lower_bounds_debug.npy', lower_bounds )
+			np.save( 'beta_debug.npy', beta )
+		
+
 		expected_fom_change = np.dot( x_star, -c )
 		print( "Expected delta = " + str( np.dot( x_star, b ) ) )
 		print( "Desired delta = " + str( self.desired_binarize_change ) )
@@ -379,7 +387,9 @@ class OptimizationLayersSpacersGlobalBinarization2DMultiDevice( OptimizationStat
 			
 			cur_reshape_idx += gradient_lengths[ bayer_idx ]
 
-			for design_idx in range( 0, self.num_devices ):
+			# todo: how to eliminate leftover names that I would expect to be out of scope coming in with typos
+			# and causing bugs (design_idx versus device_idx)
+			for device_idx in range( 0, self.num_devices ):
 				self.bayer_filters[ bayer_idx ][ device_idx ].set_design_variable( new_design_variable )
 
 