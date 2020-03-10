df1c9d7d04538ce5cbc876235c9d664446cd32bd
Gregory Roberts <gregory@Gregorys-MacBook-Pro.local>
2020-03-10 16:23:06 -0400

    fix

--- inverse_design/LayeredLithographyIRBayerFilter.py
+++ inverse_design/LayeredLithographyIRBayerFilter.py
@@ -142,14 +142,14 @@ class LayeredLithographyIRBayerFilter(device.Device):
 			original_shapes = []
 
 			for layer_start in range( 0, len( layer_start_idxs ) ):
-				extract = self.w[0][ :, :, layer_start ].flatten()
+				extract = np.real( self.w[0][ :, :, layer_start ].flatten() )
 				extract_layers.extend( extract )
 				layer_lengths.append( len( extract ) )
 
 				original_shapes.append( self.w[0][ :, :, layer_start ].shape )
 
-				extract_photonic_gradient.extend( backprop_photonic_gradient[ :, :, layer_start ].flatten() )
-				extract_binarization_gradient.extend( backprop_binarization_gradient[ :, :, layer_start ].flatten() )
+				extract_photonic_gradient.extend( np.real( backprop_photonic_gradient[ :, :, layer_start ].flatten() ) )
+				extract_binarization_gradient.extend( np.real( backprop_binarization_gradient[ :, :, layer_start ].flatten() ) )
 
 			# flatten_spacer_mask = spacer_mask.flatten()
 
