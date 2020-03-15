b05047d7ca3427348a21c8df34bdedc93ff080a9
Greg <gdrobert@caltech.edu>
2020-03-15 08:54:30 -0700

    switchable ir gsst index fix

--- inverse_design/SwitchableIRColorPolarizationParameters.py
+++ inverse_design/SwitchableIRColorPolarizationParameters.py
@@ -22,7 +22,7 @@ import FreeBayerFilter2D
 #
 # project_name = 'cmos_dielectric_2d_switchable_color_polarization_lossless_gsst_8xtsmc_um_focal_6p75um'
 # project_name = 'cmos_dielectric_2d_switchable_color_polarization_8xtsmc_um_focal_6p75um'
-project_name = 'cmos_dielectric_2d_switchable_color_polarization_lossless_gsst_just_spectral_8xtsmc_um_focal_6p75um'
+project_name = 'cmos_dielectric_2d_switchable_color_polarization_lossless_gsst_just_spectral_8xtsmc_um_focal_6p75um_test'
 
 #
 # Optical
