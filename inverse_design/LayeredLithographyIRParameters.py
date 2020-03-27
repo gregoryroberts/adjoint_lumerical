636dee2c9afe49a13ad725772ec302e674203c77
Greg <gdrobert@caltech.edu>
2020-03-27 14:26:27 -0700

    update device lateral size

--- inverse_design/LayeredLithographyIRParameters.py
+++ inverse_design/LayeredLithographyIRParameters.py
@@ -124,7 +124,7 @@ adjoint_y_positions_um = [device_size_lateral_um / 4., device_size_lateral_um / 
 #
 # Optimization
 #
-start_epoch = 6
+start_epoch = 0#6
 num_epochs = 8#8
 num_iterations_per_epoch = 60#35#50#25
 binarization_start_epoch = 1
