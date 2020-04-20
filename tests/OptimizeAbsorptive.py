a6f6e263ed1d4b597900e06d05df483f72ed4b9b
Greg <gdrobert@caltech.edu>
2020-04-20 08:32:43 -0700

    updates to color pixel

--- tests/OptimizeAbsorptive.py
+++ tests/OptimizeAbsorptive.py
@@ -365,39 +365,32 @@ lumapi_pull_results = """
 """
 
 lumapi_import_source = """
-	?'cmd 0';
 	?wl_idx;
-	?'cmd 1';
 	Ex = E_field.Ex( :, :, :, wl_idx );
-	?'cmd 2';
 	Ey = E_field.Ey( :, :, :, wl_idx );
-	?'cmd 3';
 	Ez = E_field.Ez( :, :, :, wl_idx );
-	?'cmd 4';
 	Hx = H_field.Hx( :, :, :, wl_idx );
-	?'cmd 5';
 	Hy = H_field.Hy( :, :, :, wl_idx );
-	?'cmd 6';
 	Hz = H_field.Hz( :, :, :, wl_idx );
-	?'cmd 7';
 	get_f = E_field.f( wl_idx );
-	?'cmd 8';
 	get_lambda = c / get_f;
-	?'cmd 9';
 	EM = rectilineardataset("EM fields",E_field.x,E_field.y,E_field.z);
-	?'cmd a';
 	EM.addparameter("lambda",get_lambda,"f",get_f);
-	?'cmd b';
 	EM.addattribute("E",conj(Ex),conj(Ey),conj(Ez));
-	?'cmd c';
 	EM.addattribute("H",conj(Hx),conj(Hy),conj(Hz));
-	?'cmd d';
 	switchtolayout;
-	?'cmd e';
 	select("top_adjoint_source");
-	?'cmd f';
 	importdataset(EM);
-	?'cmd 10';
+"""
+
+lumapi_import_source2 = """
+	?wl_idx;
+	Ex = E_field.Ex( :, :, :, wl_idx );
+	Ey = E_field.Ey( :, :, :, wl_idx );
+	Ez = E_field.Ez( :, :, :, wl_idx );
+	Hx = H_field.Hx( :, :, :, wl_idx );
+	Hy = H_field.Hy( :, :, :, wl_idx );
+	Hz = H_field.Hz( :, :, :, wl_idx );
 """
 
 directional_weightings_by_state = [ np.ones( num_design_frequency_points ) for idx in range( 0, num_gsst_states ) ]
@@ -447,6 +440,8 @@ for iteration in range( 0, num_iterations ):
 			''.join( lumapi_pull_results.split() )
 		)
 
+
+
 		for wl_idx in range( 0, num_design_frequency_points ):
 			print("working on adjoint idx = " + str( wl_idx ) )
 			fdtd_hook.switchtolayout()
@@ -458,9 +453,15 @@ for iteration in range( 0, num_iterations ):
 			
 			disable_all_sources()
 			top_adjoint_source.enabled = 1
-			lumapi.evalScript( fdtd_hook.handle,
-				''.join( lumapi_import_source.split() )
-			)
+
+			if wl_idx > 0:
+				lumapi.evalScript( fdtd_hook.handle,
+					''.join( lumapi_import_source2.split() )
+				)
+			else
+				lumapi.evalScript( fdtd_hook.handle,
+					''.join( lumapi_import_source.split() )
+				)
 
 			fdtd_hook.run()
 
