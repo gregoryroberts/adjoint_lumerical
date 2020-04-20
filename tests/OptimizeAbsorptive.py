e2e17198e0b8d979fd23c5fcb11f495ffa6a24af
Greg <gdrobert@caltech.edu>
2020-04-20 08:28:54 -0700

    updates to color pixel

--- tests/OptimizeAbsorptive.py
+++ tests/OptimizeAbsorptive.py
@@ -438,12 +438,12 @@ for iteration in range( 0, num_iterations ):
 			shutil.copy(
 				projects_directory_location + "/optimization.fsp",
 				projects_directory_location + "/optimization_gsst_state_" + str( gsst_state ) + ".fsp" )
-
+			
+			disable_all_sources()
+			top_adjoint_source.enabled = 1
 			lumapi.evalScript( fdtd_hook.handle,
 				''.join( lumapi_import_source.split() )
 			)
-			disable_all_sources()
-			top_adjoint_source.enabled = 1
 
 			fdtd_hook.run()
 
