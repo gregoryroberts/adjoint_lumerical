6f77c14f5b1e87f7bf78902e03e9b1d798539563
Greg <gdrobert@caltech.edu>
2020-04-22 13:08:45 -0700

    switch gsst states

--- tests/OptimizeAbsorptive.py
+++ tests/OptimizeAbsorptive.py
@@ -102,7 +102,7 @@ fdtd_region_size_lateral_um = 5
 fdtd_region_minimum_vertical_um = -2.5
 fdtd_region_maximum_vertical_um = 2.5
 
-mesh_size_um = 0.01
+mesh_size_um = 0.005
 
 fdtd_region_minimum_lateral_voxels = 1 + int ( fdtd_region_size_lateral_um / mesh_size_um )
 fdtd_region_minimum_vertical_voxels = 1 + int( ( fdtd_region_maximum_vertical_um - fdtd_region_minimum_vertical_um ) / mesh_size_um )
@@ -321,7 +321,7 @@ gsst_k_states = [ 2.5, 3.75 ]
 
 
 # note: may want an override mesh here around this interface because it is small and high index
-gsst_height_um = 0.009
+gsst_height_um = 3 * mesh_size_um
 
 num_gsst_states = len( gsst_n_states )
 
@@ -340,15 +340,15 @@ gsst_import['z max'] = 0.51 * 1e-6
 gsst_import["override mesh order from material database"] = 1
 gsst_import['mesh order'] = 1
 
-gsst_override_mesh = fdtd_hook.addmesh()
-gsst_override_mesh['name'] = 'gsst_override_mesh'
-gsst_override_mesh['x span'] = fdtd_region_size_lateral_um * 1e-6
-gsst_override_mesh['y min'] = ( gsst_min_um - 0.05 ) * 1e-6
-gsst_override_mesh['y max'] = ( gsst_max_um + 0.05 ) * 1e-6
-gsst_override_mesh['z min'] = -0.51 * 1e-6
-gsst_override_mesh['z max'] = 0.51 * 1e-6
-# gsst_override_mesh['dx'] = 0.001 * 1e-6
-gsst_override_mesh['dy'] = 0.003 * 1e-6
+# gsst_override_mesh = fdtd_hook.addmesh()
+# gsst_override_mesh['name'] = 'gsst_override_mesh'
+# gsst_override_mesh['x span'] = fdtd_region_size_lateral_um * 1e-6
+# gsst_override_mesh['y min'] = ( gsst_min_um - 0.05 ) * 1e-6
+# gsst_override_mesh['y max'] = ( gsst_max_um + 0.05 ) * 1e-6
+# gsst_override_mesh['z min'] = -0.51 * 1e-6
+# gsst_override_mesh['z max'] = 0.51 * 1e-6
+# # gsst_override_mesh['dx'] = 0.001 * 1e-6
+# gsst_override_mesh['dy'] = 0.003 * 1e-6
 
 gsst_x_range = 1e-6 * np.linspace( -0.5 * device_width_um, 0.5 * device_width_um, 2 )
 gsst_y_range = 1e-6 * np.linspace( gsst_min_um, gsst_max_um, 2 )
