868bb6d881af8117d06a548209fb8be2a69f6832
Greg <gdrobert@caltech.edu>
2020-04-26 20:00:30 -0700

    test import

--- inverse_design/Focusing2DSaveGradientDirectionsOptimizationTest.py
+++ inverse_design/Focusing2DSaveGradientDirectionsOptimizationTest.py
@@ -286,9 +286,13 @@ def import_device_permittivity( permittivity ):
 	index_from_permittivity = np.zeros( ( permittivity_shape[ 0 ], permittivity_shape[ 1 ], 2 ) )
 	index_from_permittivity[ :, :, 0 ] = index
 	index_from_permittivity[ :, :, 1 ] = index
+	print( index )
+	print( x_range )
+	print( y_range )
+	print( z_range )
 
 	fdtd_hook.switchtolayout()
-	fdtd_hook.select( 'device_import' )
+	fdtd_hook.select( device_import[ 'name' ] )
 	fdtd_hook.importnk2( index, x_range, y_range, z_range )
 
 min_feature_size_um = feature_size_meshes_um[ 0 ]
