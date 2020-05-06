74de03c9e8bac519fa8858adc042167c402f1177
Greg <gdrobert@caltech.edu>
2020-05-06 14:18:43 -0700

    lumerical cluster meory xfer test

---
+++ inverse_design/MemoryXfer.py
@@ -0,0 +1,335 @@
+import os
+import shutil
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
+
+from LayeredLithographyIRPolarizationParameters import *
+import LayeredLithographyIRPolarizationDevice
+
+import imp
+imp.load_source( "lumapi", "/central/home/gdrobert/Develompent/lumerical/2020a/api/python/lumapi.py" )
+
+import lumapi
+
+import functools
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+
+#
+# Create FDTD hook
+#
+fdtd_hook = lumapi.FDTD()
+
+#
+# Create project folder and save out the parameter file for documentation for this optimization
+#
+python_src_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
+projects_directory_location = os.path.abspath(os.path.join(os.path.dirname(__file__), '../projects/'))
+projects_directory_location += "/" + "memory_test"
+
+if not os.path.isdir(projects_directory_location):
+	os.mkdir(projects_directory_location)
+
+fdtd_hook.newproject()
+fdtd_hook.save(projects_directory_location + "/optimization")
+
+log_file = open( projects_directory_location + "/log.txt", 'w' )
+log_file.write( "Log\n" )
+log_file.close()
+
+shutil.copy2(python_src_directory + "/LayeredLithographyIRPolarizationParameters.py", projects_directory_location + "/LayeredLithographyIRPolarizationParameters.py")
+
+#
+# Set up the FDTD region and mesh
+#
+fdtd = fdtd_hook.addfdtd()
+fdtd['x span'] = fdtd_region_size_lateral_um * 1e-6
+fdtd['y span'] = fdtd_region_size_lateral_um * 1e-6
+fdtd['z max'] = fdtd_region_maximum_vertical_um * 1e-6
+fdtd['z min'] = fdtd_region_minimum_vertical_um * 1e-6
+fdtd['mesh type'] = 'uniform'
+fdtd['define x mesh by'] = 'number of mesh cells'
+fdtd['define y mesh by'] = 'number of mesh cells'
+fdtd['define z mesh by'] = 'number of mesh cells'
+fdtd['mesh cells x'] = fdtd_region_minimum_lateral_voxels
+fdtd['mesh cells y'] = fdtd_region_minimum_lateral_voxels
+fdtd['mesh cells z'] = fdtd_region_minimum_vertical_voxels
+fdtd['simulation time'] = fdtd_simulation_time_fs * 1e-15
+fdtd['background index'] = background_index
+
+#
+# General polarized source information
+#
+xy_phi_rotations = [0, 90]
+xy_names = ['x', 'y']
+
+
+#
+# Add a TFSF plane wave forward source at normal incidence
+#
+forward_sources = []
+
+for xy_idx in range(0, 2):
+	forward_src = fdtd_hook.addtfsf()
+	forward_src['name'] = 'forward_src_' + xy_names[xy_idx]
+	forward_src['angle phi'] = xy_phi_rotations[xy_idx]
+	forward_src['direction'] = 'Backward'
+	forward_src['x span'] = lateral_aperture_um * 1e-6
+	forward_src['y span'] = lateral_aperture_um * 1e-6
+	forward_src['z max'] = src_maximum_vertical_um * 1e-6
+	forward_src['z min'] = src_minimum_vertical_um * 1e-6
+	forward_src['wavelength start'] = lambda_min_um * 1e-6
+	forward_src['wavelength stop'] = lambda_max_um * 1e-6
+
+	forward_sources.append(forward_src)
+
+#
+# Place dipole adjoint sources at the focal plane that can ring in both
+# x-axis and y-axis
+#
+adjoint_sources = []
+
+for adj_src_idx in range(0, num_adjoint_sources):
+	adjoint_sources.append([])
+	for xy_idx in range(0, 2):
+		adj_src = fdtd_hook.adddipole()
+		adj_src['name'] = 'adj_src_' + str(adj_src_idx) + xy_names[xy_idx]
+		adj_src['x'] = adjoint_x_positions_um[adj_src_idx] * 1e-6
+		adj_src['y'] = adjoint_y_positions_um[adj_src_idx] * 1e-6
+		adj_src['z'] = adjoint_vertical_um * 1e-6
+		adj_src['theta'] = 90
+		adj_src['phi'] = xy_phi_rotations[xy_idx]
+		adj_src['wavelength start'] = lambda_min_um * 1e-6
+		adj_src['wavelength stop'] = lambda_max_um * 1e-6
+
+		adjoint_sources[adj_src_idx].append(adj_src)
+
+#
+# Set up the volumetric electric field monitor inside the design region.  We will need this compute
+# the adjoint gradient
+#
+design_efield_monitor = fdtd_hook.addprofile()
+design_efield_monitor['name'] = 'design_efield_monitor'
+design_efield_monitor['monitor type'] = '3D'
+design_efield_monitor['x span'] = device_size_lateral_um * 1e-6
+design_efield_monitor['y span'] = device_size_lateral_um * 1e-6
+design_efield_monitor['z max'] = device_vertical_maximum_um * 1e-6
+design_efield_monitor['z min'] = device_vertical_minimum_um * 1e-6
+design_efield_monitor['override global monitor settings'] = 1
+design_efield_monitor['use wavelength spacing'] = 1
+design_efield_monitor['use source limits'] = 1
+design_efield_monitor['frequency points'] = num_design_frequency_points
+design_efield_monitor['output Hx'] = 0
+design_efield_monitor['output Hy'] = 0
+design_efield_monitor['output Hz'] = 0
+
+#
+# Set up adjoint point monitors to get electric field strength at focus spots.  This will allow us to
+# compute the figure of merit as well as weight the adjoint simulations properly in calculation of the
+# gradient.
+#
+focal_monitors = []
+
+for adj_src in range(0, num_adjoint_sources):
+	focal_monitor = fdtd_hook.addpower()
+	focal_monitor['name'] = 'focal_monitor_' + str(adj_src)
+	focal_monitor['monitor type'] = 'point'
+	focal_monitor['x'] = adjoint_x_positions_um[adj_src] * 1e-6
+	focal_monitor['y'] = adjoint_y_positions_um[adj_src] * 1e-6
+	focal_monitor['z'] = adjoint_vertical_um * 1e-6
+	focal_monitor['override global monitor settings'] = 1
+	focal_monitor['use wavelength spacing'] = 1
+	focal_monitor['use source limits'] = 1
+	focal_monitor['frequency points'] = num_design_frequency_points
+
+	focal_monitors.append(focal_monitor)
+
+
+transmission_focal_monitors = []
+
+for adj_src in range(0, num_adjoint_sources):
+	transmission_focal_monitor = fdtd_hook.addpower()
+	transmission_focal_monitor['name'] = 'transmission_focal_monitor_' + str(adj_src)
+	transmission_focal_monitor['monitor type'] = '2D Z-Normal'
+	transmission_focal_monitor['x'] = adjoint_x_positions_um[adj_src] * 1e-6
+	transmission_focal_monitor['x span'] = 0.5 * device_size_lateral_um * 1e-6
+	transmission_focal_monitor['y'] = adjoint_y_positions_um[adj_src] * 1e-6
+	transmission_focal_monitor['y span'] = 0.5 * device_size_lateral_um * 1e-6
+	transmission_focal_monitor['z'] = adjoint_vertical_um * 1e-6
+	transmission_focal_monitor['override global monitor settings'] = 1
+	transmission_focal_monitor['use wavelength spacing'] = 1
+	transmission_focal_monitor['use source limits'] = 1
+	transmission_focal_monitor['frequency points'] = num_eval_frequency_points
+	transmission_focal_monitor.enabled = 0
+
+	transmission_focal_monitors.append(transmission_focal_monitor)
+
+transmission_focal = fdtd_hook.addpower()
+transmission_focal['name'] = 'transmission_focal'
+transmission_focal['monitor type'] = '2D Z-Normal'
+transmission_focal['x'] = 0 * 1e-6
+transmission_focal['x span'] = device_size_lateral_um * 1e-6
+transmission_focal['y'] = 0 * 1e-6
+transmission_focal['y span'] = device_size_lateral_um * 1e-6
+transmission_focal['z'] = adjoint_vertical_um * 1e-6
+transmission_focal['override global monitor settings'] = 1
+transmission_focal['use wavelength spacing'] = 1
+transmission_focal['use source limits'] = 1
+transmission_focal['frequency points'] = num_eval_frequency_points
+transmission_focal.enabled = 0
+
+
+#
+# Add SiO2 at the top
+#
+sio2_top = fdtd_hook.addrect()
+sio2_top['name'] = 'sio2_top'
+sio2_top['x span'] = fdtd_region_size_lateral_um * 1e-6
+sio2_top['y span'] = fdtd_region_size_lateral_um * 1e-6
+sio2_top['z min'] = device_vertical_maximum_um * 1e-6
+sio2_top['z max'] = fdtd_region_maximum_vertical_um * 1e-6
+sio2_top['index'] = index_sio2
+
+air_bottom = fdtd_hook.addrect()
+air_bottom['name'] = 'air_bottom'
+air_bottom['x span'] = fdtd_region_size_lateral_um * 1e-6
+air_bottom['y span'] = fdtd_region_size_lateral_um * 1e-6
+air_bottom['z min'] = fdtd_region_minimum_vertical_um * 1e-6
+air_bottom['z max'] = device_vertical_minimum_um * 1e-6
+air_bottom['index'] = index_air
+
+
+#
+# Add device region and create device permittivity
+#
+design_import = fdtd_hook.addimport()
+design_import['name'] = 'design_import'
+design_import['x span'] = device_size_lateral_um * 1e-6
+design_import['y span'] = device_size_lateral_um * 1e-6
+design_import['z max'] = device_vertical_maximum_um * 1e-6
+design_import['z min'] = device_vertical_minimum_um * 1e-6
+
+bayer_filter_size_voxels = np.array([device_voxels_lateral, device_voxels_lateral, device_voxels_vertical])
+bayer_filter = LayeredLithographyIRPolarizationDevice.LayeredLithographyIRPolarizationDevice(
+	bayer_filter_size_voxels,
+	[min_device_permittivity, max_device_permittivity],
+	init_permittivity_0_1_scale,
+	num_vertical_layers,
+	spacer_size_voxels,
+	[index_air**2, index_silicon**2],
+	max_binarize_movement,
+	desired_binarize_change)
+
+
+# bayer_filter.set_design_variable( np.random.random( bayer_filter.get_design_variable().shape ) )
+# bayer_filter.step(
+# 	np.random.random( bayer_filter.get_design_variable().shape ),
+# 	0.01,
+# 	True,
+# 	projects_directory_location
+# )
+# sys.exit(0)
+
+# bayer_filter.set_design_variable( np.load(projects_directory_location + "/cur_design_variable.npy") )
+
+bayer_filter_region_x = 1e-6 * np.linspace(-0.5 * device_size_lateral_um, 0.5 * device_size_lateral_um, device_voxels_lateral)
+bayer_filter_region_y = 1e-6 * np.linspace(-0.5 * device_size_lateral_um, 0.5 * device_size_lateral_um, device_voxels_lateral)
+bayer_filter_region_z = 1e-6 * np.linspace(device_vertical_minimum_um, device_vertical_maximum_um, device_voxels_vertical)
+
+#
+# Disable all sources in the simulation, so that we can selectively turn single sources on at a time
+#
+def disable_all_sources():
+	fdtd_hook.switchtolayout()
+
+	for xy_idx in range(0, 2):
+		(forward_sources[xy_idx]).enabled = 0
+
+	for adj_src_idx in range(0, num_adjoint_sources):
+		for xy_idx in range(0, 2):
+			(adjoint_sources[adj_src_idx][xy_idx]).enabled = 0
+
+#
+# Consolidate the data transfer functionality for getting data from Lumerical FDTD process to
+# python process.  This is much faster than going through Lumerical's interop library
+#
+def get_monitor_data(monitor_name, monitor_field):
+	lumerical_data_name = "monitor_data_" + monitor_name + "_" + monitor_field
+	extracted_data_name = lumerical_data_name + "_data"
+	data_transfer_filename = projects_directory_location + "/data_transfer_" + monitor_name + "_" + monitor_field
+
+	command_read_monitor = lumerical_data_name + " = getresult(\'" + monitor_name + "\', \'" + monitor_field + "\');"
+	command_extract_data = extracted_data_name + " = " + lumerical_data_name + "." + monitor_field + ";"
+	command_save_data_to_file = "matlabsave(\'" + data_transfer_filename + "\', " + extracted_data_name + ");"
+
+	lumapi.evalScript(fdtd_hook.handle, command_read_monitor)
+	lumapi.evalScript(fdtd_hook.handle, command_extract_data)
+
+	# start_time = time.time()
+
+	lumapi.evalScript(fdtd_hook.handle, command_save_data_to_file)
+	monitor_data = {}
+	load_file = h5py.File(data_transfer_filename + ".mat")
+
+	monitor_data = np.array(load_file[extracted_data_name])
+
+	# end_time = time.time()
+
+	# print("\nIt took " + str(end_time - start_time) + " seconds to transfer the monitor data\n")
+
+	return monitor_data
+
+def get_complex_monitor_data(monitor_name, monitor_field):
+	data = get_monitor_data(monitor_name, monitor_field)
+	return (data['real'] + np.complex(0, 1) * data['imag'])
+
+disable_all_sources()
+forward_sources[0].enabled = 0
+
+fdtd_hook.run()
+
+num_xfers = 1
+
+start = time.time()
+
+data_size_GB = 0
+
+for xfer in range( 0, num_xfers ):
+	data = get_complex_monitor_data( design_efield_monitor[ 'name' ], 'E' )
+	data_size = data.nbytes / ( 1024. * 1024. * 1024. )
+
+elapsed = time.time() - start
+
+log_file = open( projects_directory_location + "/log.txt", 'a' )
+log_file.write( "Average time for to/from disk: " + str( elapsed / num_xfers ) + " over " + str( num_xfers ) + " xfers\n" )
+log_file.write( "The data size is: " + str( data_size_GB ) + " GB\n" )
+log_file.write( "Average data rate for xfer is: " + str( 1024. * data_size_GB / ( elapsed / num_xfers ) ) + " MB/s\n" )
+log_file.close()
+
+data_size_GB = 0
+
+for xfer in range( 0, num_xfers ):
+	lumerical_data_name = "monitor_data_" + monitor_name + "_" + monitor_field
+	extracted_data_name = lumerical_data_name + "_data"
+
+	command_read_monitor = lumerical_data_name + " = getresult(\'" + monitor_name + "\', \'" + monitor_field + "\');"
+	command_extract_data = extracted_data_name + " = " + lumerical_data_name + "." + monitor_field + ";"
+
+	lumapi.evalScript(fdtd_hook.handle, command_read_monitor)
+	lumapi.evalScript(fdtd_hook.handle, command_extract_data)
+
+	data = lumapi.getv( extracted_data_name )
+	data_size = data.nbytes / ( 1024. * 1024. * 1024. )
+
+elapsed = time.time() - start
+
+log_file = open( projects_directory_location + "/log.txt", 'a' )
+log_file.write( "Average time for getv method: " + str( elapsed / num_xfers ) + " over " + str( num_xfers ) + " xfers\n" )
+log_file.write( "The data size is: " + str( data_size_GB ) + " GB\n" )
+log_file.write( "Average data rate for xfer is: " + str( 1024. * data_size_GB / ( elapsed / num_xfers ) ) + " MB/s\n" )
+log_file.close()
+
