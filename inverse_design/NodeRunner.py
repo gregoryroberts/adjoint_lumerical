2ff9cce85bb37abc7adc18852554b52ac2bbe629
Greg <gdrobert@caltech.edu>
2020-05-08 11:59:34 -0700

    logs

--- inverse_design/NodeRunner.py
+++ inverse_design/NodeRunner.py
@@ -16,6 +16,11 @@ id_number = sys.argv[ 2 ]
 
 pattern = re.compile("ID" + id_number + "(.*?)\.READY")
 
+log_filename = '/central/home/gdrobert/Develompent/adjoint_lumerical/inverse_design/log' + id_number + '.txt'
+log_file = open( log_filename, 'w' )
+log_file.write("Log...\n")
+log_file.close()
+
 while True:
     
     if os.path.isdir( filebase ):
@@ -23,9 +28,11 @@ while True:
             if pattern.match( filepath ):
 
                 file_root = filebase + "/" + filepath[:-5]
-                print("File root is...")
-                print(file_root)
-                print("File root was...")
+                log_file = open( log_filename, 'a' )
+                log_file.write("File root is...\n")
+                log_file.write(file_root)
+                log_file.write("\n")
+                log_file.close()
                 os.remove( file_root + "READY" )
                 
                 lumerical_bin_mpiexec = "/central/home/gdrobert/Develompent/lumerical/2020a_r6/mpich2/nemesis/bin/"
