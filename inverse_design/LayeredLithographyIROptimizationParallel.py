89cd107857287bd65f02fa60cbfd27c08decebfe
Greg <gdrobert@caltech.edu>
2020-06-09 13:11:00 -0700

    try out interpolated data

--- inverse_design/LayeredLithographyIROptimizationParallel.py
+++ inverse_design/LayeredLithographyIROptimizationParallel.py
@@ -371,7 +371,7 @@ def get_efield_interpolated( monitor_name, spatial_limits_um, new_size ):
 	start = time.time()
 
 	for coord_idx in range( 0, len( spatial_limits_um ) ):
-		command_setup_new_coord = "new_coord_space_" + str( coord_idx ) + " = 1e-6 * linspace( " + spatial_limits_um[ coord_idx ][ 0 ] + ", " + spatial_limits_um[ coord_idx ][ 1 ] + ", " + str( new_size[ coord_idx ] ) + " ):"
+		command_setup_new_coord = "new_coord_space_" + str( coord_idx ) + " = 1e-6 * linspace( " + str( spatial_limits_um[ coord_idx ][ 0 ] ) + ", " + str( spatial_limits_um[ coord_idx ][ 1 ] ) + ", " + str( new_size[ coord_idx ] ) + " ):"
 		lumapi.evalScript(fdtd_hook.handle, command_setup_new_coord)
 
 	total_efield = np.zeros( [ len (field_polariations ) ] + list( new_size ), dtype=np.complex )
