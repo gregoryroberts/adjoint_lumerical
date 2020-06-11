b8ec0e31cd70f037924f2f99798f906e1e436c10
Greg <gdrobert@caltech.edu>
2020-06-11 06:58:12 -0700

    fix

--- inverse_design/SingleLayerIRCircularSplitterOptimizationParallelLSF.py
+++ inverse_design/SingleLayerIRCircularSplitterOptimizationParallelLSF.py
@@ -131,7 +131,7 @@ python_src_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '
 projects_directory_location = "/central/groups/Faraon_Computing/projects" 
 projects_init_design_directory = projects_directory_location + "/" + project_name + '_parallel'# + '_parallel_symmetric_lsf_v2'
 
-projects_directory_location += "/" + project_name + '_parallel_symmetric_lsf_v3'
+projects_directory_location += "/" + project_name + '_parallel_symmetric_lsf_v3_neg'
 
 if not os.path.isdir(projects_directory_location):
 	os.mkdir(projects_directory_location)
