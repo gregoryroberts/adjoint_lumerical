de6f3ce3b0d7c6b9dc3d96cad58addae46b61703
Greg <gdrobert@caltech.edu>
2020-07-17 10:53:46 -0700

    spacer games

--- inverse_design/LayeredLithographyIRParameters.py
+++ inverse_design/LayeredLithographyIRParameters.py
@@ -90,9 +90,9 @@ spacer_size_um = ( device_size_verical_um / num_vertical_layers ) - amorphous_si
 simulated_device_voxels_lateral = 2 + int(device_size_lateral_um / mesh_spacing_um)
 simulated_device_voxels_vertical = 2 + int(device_size_verical_um / mesh_spacing_um)
 
-device_voxels_lateral = 2 + int(device_size_lateral_um / design_spacing_um)
+device_voxels_lateral = int(device_size_lateral_um / design_spacing_um)
 # device_voxels_lateral = 1 + int(device_size_lateral_um / mesh_spacing_um)
-device_voxels_vertical = 2 + int(device_size_verical_um / design_spacing_um)
+device_voxels_vertical = int(device_size_verical_um / design_spacing_um)
 # device_voxels_vertical = 1 + int(device_size_verical_um / mesh_spacing_um)
 spacer_size_voxels = int(spacer_size_um / design_spacing_um)
 
