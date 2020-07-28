e29694ed36ec71f7622aba8ceaa54f4e688c9b5e
Greg <gdrobert@caltech.edu>
2020-07-28 14:26:42 -0700

    try control points

---
+++ inverse_design/LayeredLithographyAMBayerFilterCtrlPts.py
@@ -0,0 +1,271 @@
+import device as device
+import layering as layering
+import scale as scale
+import sigmoid as sigmoid
+import square_blur as square_blur
+
+from scipy.ndimage import gaussian_filter
+
+import numpy as np
+
+from LayeredLithographyAMCtrlPtsParameters import *
+
+class GaussianBlur():
+	def __init__( self, blur_sigma ):
+		self.blur_sigma = blur_sigma
+
+	def forward( self, variable_in ):
+		# return variable_in
+		z_shape = variable_in.shape[ 2 ]
+
+		variable_out = np.zeros( variable_in.shape, dtype=variable_in.dtype )
+
+		for z_idx in range( 0, z_shape ):
+			get_layer = np.squeeze( variable_in[ :, :, z_idx ] )
+			# blurred_layer = 2 * np.pi * self.blur_sigma**2 * gaussian_filter( np.real( get_layer ), sigma=self.blur_sigma )
+			blurred_layer = gaussian_filter( np.real( get_layer ), sigma=self.blur_sigma )
+
+			variable_out[ :, :, z_idx ] = blurred_layer
+
+		return np.maximum( np.minimum( variable_out, 1.0 ), 0.0 )
+
+	def chain_rule( self, gradient_out, variable_out, variable_in ):
+		# return gradient_out
+		z_shape = gradient_out.shape[ 2 ]
+
+		gradient_in = np.zeros( gradient_out.shape, dtype=gradient_out.dtype )
+
+		for z_idx in range( 0, z_shape ):
+			get_layer = np.squeeze( gradient_out[ :, :, z_idx ] )
+			# blurred_layer = 2 * np.pi * self.blur_sigma**2 * gaussian_filter( np.real( get_layer ), sigma=self.blur_sigma )
+			blurred_layer = gaussian_filter( np.real( get_layer ), sigma=self.blur_sigma )
+
+			gradient_in[ :, :, z_idx ] = blurred_layer
+
+		return gradient_in	
+
+class LayeredLithographyAMBayerFilterCtrlPts(device.Device):
+
+	def __init__(self, size, lateral_subsampling, feature_size_sigma, permittivity_bounds, init_permittivity, num_z_layers, spacer_height_voxels):
+		super(LayeredLithographyAMBayerFilterCtrlPts, self).__init__(size, permittivity_bounds, init_permittivity)
+
+		self.num_z_layers = num_z_layers
+		self.flip_threshold = 0.5
+		self.minimum_design_value = 0
+		self.maximum_design_value = 1
+		self.spacer_height_voxels = spacer_height_voxels
+		self.layer_height_voxels = int( ( self.size[ 2 ] / self.num_z_layers ) - self.spacer_height_voxels )
+		self.init_filters_and_variables()
+
+		self.lateral_subsampling = lateral_subsampling
+
+		self.feature_size_sigma = feature_size_sigma
+		self.control_points_per_box = 2
+		self.box_counts = np.zeros( 2 )
+		for dim in range( 0, 2 ):
+			# todo: these should be errors or exceptions or some return code, not assert
+			assert ( size[ dim ] % lateral_subsampling ) == 0, "The subsampling does not divide the size equally"
+			self.box_counts[ dim ] = int( size[ dim ] / lateral_subsampling[ dim ] )
+
+		self.init_control_boxes()
+		
+		self.update_permittivity()
+
+	#
+	# todo: Swapping control points maintains the same distribution.  This may affect the convergence of this and could be
+	# something to consider.
+	#
+	def init_control_boxes( self ):
+		self.control_points = np.zeros( ( self.num_z_layers, self.box_counts[ 0 ], self.box_counts[ 1 ], self.control_points_per_box, 2 ) )
+
+		for layer_idx in range( 0, self.num_z_layers ):
+			for xbox in range( 0, self.box_counts[ 0 ] ):
+				for ybox in range( 0, self.box_counts[ 1 ] ):
+
+					offset_x = xbox * self.lateral_subsampling[ 0 ] + int( 0.25 * self.lateral_subsampling[ 0 ] )
+					offset_y = ybox * self.lateral_subsampling[ 1 ] + int( 0.25 * self.lateral_subsampling[ 1 ] )
+
+					random_control_pts_x = np.random.random( self.control_points_per_box ) * 0.5 * self.lateral_subsampling[ 0 ]
+					random_control_pts_y = np.random.random( self.control_points_per_box ) * 0.5 * self.lateral_subsampling[ 1 ]
+
+					for pt_idx in range( 0, self.control_points_per_box ):
+						self.control_points[ layer_idx, xbox, ybox, pt_idx ] = np.array( [ offset_x + random_control_pts_x[ pt_idx ], offset_y + random_control_pts_y[ pt_idx ] ] )
+
+
+	def make_layer( self, layer_idx ):
+		layer = np.zeros( ( fine_dim, fine_dim ) )
+
+		for x_coarse in range( 0, coarse_dim ):
+			fine_x_offset = self.lateral_subsampling[ 0 ] * x_coarse
+
+			for y_coarse in range( 0, coarse_dim ):
+				fine_y_offset = self.lateral_subsampling[ 1 ] * y_coarse
+
+				for step_x in range( 0, fine_factor ):
+					fine_x = fine_x_offset + step_x
+
+					for step_y in range( 0, fine_factor ):
+						fine_y = fine_y_offset + step_y
+
+						for pt_idx in range( 0, num_ctrl_pts_per_box ):
+							ctrl_x = self.control_points[ layer_idx, x_coarse, y_coarse, pt_idx, 0 ]
+							ctrl_y = self.control_points[ layer_idx, x_coarse, y_coarse, pt_idx, 1 ]
+
+							layer[ fine_x, fine_y ] += np.exp( -( 1. / ( 2. * self.feature_size_sigma**2 ) ) * ( ( ( fine_x - ctrl_x )**2 + ( fine_y - ctrl_y )**2 ) ) )
+
+		return layer
+
+	def layer_to_control_points_grad( self, layer_idx, gradient ):
+		control_points_grad = np.zeros( self.control_points.shape, dtype=self.control_points.dtype )
+
+		for x_coarse in range( 0, coarse_dim ):
+			fine_x_offset = self.lateral_subsampling[ 0 ] * x_coarse
+
+			for y_coarse in range( 0, coarse_dim ):
+				fine_y_offset = self.lateral_subsampling[ 1 ] * y_coarse
+
+				for step_x in range( 0, fine_factor ):
+					fine_x = fine_x_offset + step_x
+
+					for step_y in range( 0, fine_factor ):
+						fine_y = fine_y_offset + step_y
+
+						for pt_idx in range( 0, num_ctrl_pts_per_box ):
+							ctrl_x = self.control_points[ layer_idx, x_coarse, y_coarse, pt_idx, 0 ]
+							ctrl_y = self.control_points[ layer_idx, x_coarse, y_coarse, pt_idx, 1 ]
+
+							control_points_grad[ x_coarse, y_coarse, pt_idx, 0 ] += (
+								gradient[ fine_x, fine_y ] *
+								np.exp( -( 1. / ( 2. * self.feature_size_sigma**2 ) ) * ( ( ( fine_x - ctrl_x )**2 + ( fine_y - ctrl_y )**2 ) ) ) *
+								( 1. / self.feature_size_sigma**2 ) *
+								( fine_x - ctrl_x )
+							)
+
+							control_points_grad[ x_coarse, y_coarse, pt_idx, 1 ] += (
+								gradient[ fine_x, fine_y ] *
+								np.exp( -( 1. / ( 2. * self.feature_size_sigma**2 ) ) * ( ( ( fine_x - ctrl_x )**2 + ( fine_y - ctrl_y )**2 ) ) ) *
+								( 1. / self.feature_size_sigma**2 ) *
+								( fine_y - ctrl_y )
+							)
+
+		return control_points_grad
+
+	#
+	# Override the update_permittivity function so we can handle layer-dependent collapsing along either x- or y-dimensions
+	#
+	def update_permittivity( self ):
+		background_density = np.ones( self.size )
+
+		layer_indices = self.layering_z.get_layer_idxs( self.size )
+
+		for layer_idx in range( 0, self.num_z_layers ):
+			layer_start = layer_indices[ layer_idx ]
+			layer_end = np.minimum( self.size[ 2 ] - self.spacer_height_voxels, layer_start + self.layer_height_voxels )
+
+			create_layer = self.make_layer( layer_idx )
+
+			for internal_idx in range( layer_start, layer_end ):
+				background_density[ :, :, internal_idx ] = create_layer
+
+
+		sigmoid_density = self.sigmoid_1.forward( background_density )
+		scale_density = self.scale_2.forward( sigmoid_density )
+
+		self.w[ 1 ] = background_density
+		self.w[ 2 ] = sigmoid_density
+		self.w[ 3 ] = scale_density
+
+
+	#
+	# Need to also override the backpropagation function
+	#
+	def backpropagate( self, gradient ):
+		gradient = self.scale_2.chain_rule( gradient, self.w[ 3 ], self.w[ 2 ] )
+		gradient = self.sigmoid_1.chain_rule( gradient, self.w[ 2 ], self.w[ 1 ] )
+
+		layer_indices = self.layering_z.get_layer_idxs( self.size )
+
+		average_gradient_layers = np.zeros( ( self.size[ 0 ], self.size[ 1 ], self.num_z_layers ), dtype=gradient.dtype )
+
+		control_points_gradient = np.zeros( ( self.num_z_layers, self.box_counts[ 0 ], self.box_counts[ 1 ], self.control_points_per_box, 2 ) )
+
+		for layer_idx in range( 0, self.num_z_layers ):
+			layer_start = layer_indices[ layer_idx ]
+			layer_end = np.minimum( self.size[ 2 ] - self.spacer_height_voxels, layer_start + self.layer_height_voxels )
+
+			average_gradient_layer = np.zeros( ( self.size[ 0 ], self.size[ 1 ] ), dtype=gradient.dtype )
+			num_voxels_averaged = layer_end - layer_start
+			averaging_normalization = 1. / num_voxels_averaged
+
+			for internal_idx in range( layer_start, layer_end ):
+				average_gradient_layers[ :, :, layer_idx ] += ( averaging_normalization * gradient[ internal_idx ] )
+
+			control_points_gradient[ layer_idx ] = self.layer_to_control_points_grad( layer_idx, average_gradient_layers[ :, :, layer_idx ] )
+
+		return control_points_gradient
+
+	def update_filters(self, epoch):
+		self.sigmoid_beta = 0.0625 * ( 2**epoch )
+
+		self.sigmoid_1 = sigmoid.Sigmoid( self.sigmoid_beta, self.sigmoid_eta )
+
+	def init_filters_and_variables(self):
+		self.num_filters = 3
+		self.num_variables = 1 + self.num_filters
+
+		# Start the sigmoids at weak strengths
+		self.sigmoid_beta = 0.0625
+		self.sigmoid_eta = 0.5
+		self.sigmoid_1 = sigmoid.Sigmoid(self.sigmoid_beta, self.sigmoid_eta)
+
+		x_dimension_idx = 0
+		y_dimension_idx = 1
+		z_dimension_idx = 2
+
+		z_voxel_layers = self.size[2]
+		self.layering_z = layering.Layering( z_dimension_idx, self.num_z_layers, [ 0, 1 ], self.spacer_height_voxels, 0.0 )
+
+		scale_min = self.permittivity_bounds[0]
+		scale_max = self.permittivity_bounds[1]
+		self.scale_1 = scale.Scale([scale_min, scale_max])
+
+		self.init_variables()
+
+	# In the step function, we should update the permittivity with update_permittivity
+	def step(self, gradient, step_size):
+		control_points_gradient = self.backpropagate( gradient )
+		proposed_step = self.control_points - step_size * control_points_gradient
+
+
+		for layer_idx in range( 0, self.num_z_layers ):
+			for x_coarse in range( 0, coarse_dim ):
+				x_bound_low = self.lateral_subsampling[ 0 ] * ( x_coarse + 0.25 )
+				x_bound_high = self.lateral_subsampling[ 0 ] * ( x_coarse + 0.75 )
+
+				for y_coarse in range( 0, coarse_dim ):
+					y_bound_low = self.lateral_subsampling[ 1 ] * ( y_coarse + 0.25 )
+					y_bound_high = self.lateral_subsampling[ 1 ] * ( y_coarse + 0.75 )
+
+					for pt_idx in range( 0, num_ctrl_pts_per_box ):
+						self.proposed_step[ layer_idx, x_coarse, y_coarse, pt_idx, 0 ] = np.minimum(
+							x_bound_high,
+							np.maximum(
+								self.proposed_step[ layer_idx, x_coarse, y_coarse, pt_idx, 0 ],
+								x_bound_low )
+							)
+
+						self.proposed_step[ layer_idx, x_coarse, y_coarse, pt_idx, 1 ] = np.minimum(
+							y_bound_high,
+							np.maximum(
+								self.proposed_step[ layer_idx, x_coarse, y_coarse, pt_idx, 1 ],
+								y_bound_low )
+							)
+
+		self.control_points = self.proposed_step.copy()
+
+		# self.w[0] = self.proposed_design_step(gradient, step_size)
+		# Update the variable stack including getting the permittivity at the w[-1] position
+		self.update_permittivity()
+
+	def convert_to_binary_map(self, variable):
+		return np.greater(variable, self.mid_permittivity)
---
+++ inverse_design/LayeredLithographyAMCtrlPtsParameters.py
@@ -0,0 +1,188 @@
+#
+# Parameter file for the Bayer Filter Layered Lithography optimization
+#
+
+import numpy as np
+
+#
+# Files
+#
+project_name = 'layered_rgb_5layers_pol_insensitive_ctrl_pts_2x2x4um_weighting_f1p5'
+
+#
+# Optical
+#
+# averaging between the part where we dip into the infrared/red part of the spectrum
+# and where we are in the telecom ranges
+#
+index_silicon = 0.5 * ( 3.47 + 3.86 )
+index_am = 2.4
+index_substrate = 1.451
+index_sio2 = 1.46
+index_air = 1.0
+background_index = index_sio2
+min_device_index = index_sio2
+max_device_index = index_am
+
+# last_layer_permittivities = [ index_tio2**2, index_zep**2 ]
+# last_layer_permittivities = [ index_sio2**2, index_am**2 ]
+
+min_device_permittivity = min_device_index**2
+max_device_permittivity = max_device_index**2
+
+random_seed = 6235234
+init_permittivity_0_1_scale = 0.25
+
+focal_length_um = 1.5
+# focal_length_um = 3.0
+focal_plane_center_lateral_um = 0
+focal_plane_center_vertical_um = -focal_length_um
+
+#
+# Device
+#
+mesh_spacing_um = 0.02
+# mesh_spacing_um = 0.04
+# For now, we will put the blurring out to 60nm in the filter chain
+design_spacing_um = 0.02
+# design_spacing_um = 0.04
+
+lateral_box_size_um = 0.2
+lateral_box_sampling = int( lateral_box_size_um / mesh_spacing_um )
+
+
+
+device_border_um = 0.12
+# device_border_um = 0.24
+device_border_voxels = int( device_border_um / mesh_spacing_um )
+
+#
+# Pesky size to get the number of voxels to be a multiple of 3
+#
+num_vertical_layers = 5
+
+device_size_lateral_um = 2.0
+# device_size_lateral_um = 4.0
+
+device_size_verical_um = num_vertical_layers * 0.8
+# device_size_verical_um = num_vertical_layers * 1.0
+
+device_height_per_layer_um = 0.4
+# device_height_per_layer_um = 0.5
+spacer_size_um = ( device_size_verical_um / num_vertical_layers ) - device_height_per_layer_um
+
+simulated_device_voxels_lateral = int(device_size_lateral_um / mesh_spacing_um)
+simulated_device_voxels_vertical = int(device_size_verical_um / mesh_spacing_um)
+
+device_voxels_lateral = int(device_size_lateral_um / design_spacing_um)
+device_voxels_vertical = int(device_size_verical_um / design_spacing_um)
+spacer_size_voxels = int(spacer_size_um / design_spacing_um)
+
+device_vertical_maximum_um = device_size_verical_um
+device_vertical_minimum_um = 0
+
+#
+# Spectral
+#
+lambda_min_um = 0.4
+lambda_max_um = 0.7
+# lambda_min_um = 0.8
+# lambda_max_um = 1.4
+
+num_bands = 3
+num_points_per_band = 10
+num_design_frequency_points = num_bands * num_points_per_band
+num_eval_frequency_points = 60
+
+lambda_values_um = np.linspace(lambda_min_um, lambda_max_um, num_design_frequency_points)
+max_intensity_by_wavelength = (device_size_lateral_um**2)**2 / (focal_length_um**2 * lambda_values_um**2)
+
+#
+# Fabrication Constraints
+#
+max_blur_filter_size_um = 0.06
+# gaussian_blur_size_um = 0.1
+gaussian_blur_size_um = 0.1 / np.sqrt( 2 * np.log( 2 ) )
+
+max_blur_filter_half_width = int( ( ( max_blur_filter_size_um / design_spacing_um ) - 1 ) / 2 )
+# max_blur_filter_half_width = 0#int( ( ( max_blur_filter_size_um / design_spacing_um ) - 1 ) / 2 )
+# gaussian_blur_filter_sigma = int( ( ( gaussian_blur_size_um / design_spacing_um ) - 1 ) / 2 )
+gaussian_blur_filter_sigma = ( ( ( gaussian_blur_size_um / design_spacing_um ) - 1 ) / 2 )
+
+#
+# FDTD
+#
+vertical_gap_size_um = lambda_max_um
+lateral_gap_size_um = 0.5 * device_size_lateral_um
+
+fdtd_region_size_vertical_um = 2 * vertical_gap_size_um + device_size_verical_um + focal_length_um
+fdtd_region_size_lateral_um = 2 * lateral_gap_size_um + device_size_lateral_um
+fdtd_region_maximum_vertical_um = device_size_verical_um + vertical_gap_size_um
+fdtd_region_minimum_vertical_um = -focal_length_um - vertical_gap_size_um
+
+fdtd_region_minimum_vertical_voxels = int( np.ceil(fdtd_region_size_vertical_um / mesh_spacing_um) )
+fdtd_region_minimum_lateral_voxels = int( np.ceil(fdtd_region_size_lateral_um / mesh_spacing_um) )
+
+import_region_voxels_lateral = 1 + int( fdtd_region_size_vertical_um / design_spacing_um )
+
+fdtd_simulation_time_fs = 8 * 700
+
+#
+# Forward Source
+#
+lateral_aperture_um = device_size_lateral_um + 0.5 * lateral_gap_size_um
+src_maximum_vertical_um = device_size_verical_um + 0.5 * vertical_gap_size_um
+src_minimum_vertical_um = -focal_length_um - 0.5 * vertical_gap_size_um
+
+#
+# Spectral and polarization selectivity information
+#
+polarizations_focal_plane_map = [ ['x', 'y'], ['x', 'y'], ['x', 'y'], ['x', 'y'] ]
+weight_focal_plane_map = [ 1.0, 1.0, 1.0, 1.0 ]
+polarization_name_to_idx = { 'x':0, 'y':1, 'z':2 }
+# We are assuming that the data is organized in order of increasing wavelength (i.e. - blue first, red last)
+spectral_focal_plane_map = [
+	[0, num_points_per_band],
+	[num_points_per_band, 2 * num_points_per_band],
+	[2 * num_points_per_band, 3 * num_points_per_band],
+	[num_points_per_band, 2 * num_points_per_band]
+]
+
+#
+# Adjoint sources
+#
+adjoint_vertical_um = -focal_length_um
+num_focal_spots = 4
+num_adjoint_sources = num_focal_spots
+adjoint_x_positions_um = [device_size_lateral_um / 4., -device_size_lateral_um / 4., -device_size_lateral_um / 4., device_size_lateral_um / 4.]
+adjoint_y_positions_um = [device_size_lateral_um / 4., device_size_lateral_um / 4., -device_size_lateral_um / 4., -device_size_lateral_um / 4.]
+
+#
+# Symmetry
+#
+forward_symmetry = [ 'y', 'x' ]
+adjoint_symmetry_location = [ 0, 3, 2, 1 ]
+adjoint_symmetry_pol = [ 'y', 'x' ]
+
+#
+# Optimization
+#
+start_epoch = 0#12#7#6
+num_epochs = 10#14
+num_iterations_per_epoch = 50#35#50#25
+# binarization_start_epoch = 2#1#4
+# max_binarize_movement = 0.0025
+# desired_binarize_change = 0.005
+
+# Probably need to taper this over the epochs!
+ctrl_pts_change_start_epoch = 0.5
+ctrl_pts_change_end_epoch = 0.1
+
+# epoch_start_permittivity_change_max = 0.05#0.1
+# epoch_end_permittivity_change_max = 0.01#0.02
+# epoch_range_permittivity_change_max = epoch_start_permittivity_change_max - epoch_end_permittivity_change_max
+
+# epoch_start_permittivity_change_min = 0.025#0.05
+# epoch_end_permittivity_change_min = 0
+# epoch_range_permittivity_change_min = epoch_start_permittivity_change_min - epoch_end_permittivity_change_min
+
