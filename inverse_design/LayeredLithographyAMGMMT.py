42a68b50141eb56a0a0d50f8a0593e2f2ff360d1
Greg <gdrobert@caltech.edu>
2020-08-06 18:15:57 -0700

    gmmt

--- inverse_design/LayeredLithographyAMGMMT.py
+++ inverse_design/LayeredLithographyAMGMMT.py
@@ -544,8 +544,7 @@ while comparison < num_comparisons:
 					position=get_center,
 					refractive_index=random_indices[ sphere_idx ],
 					radius=( random_radii[ sphere_idx ] / nm ),
-					lmax=l_max )
-				)
+					lmax=l_max ) )
 
 		mie_start = time.time()
 
@@ -556,46 +555,46 @@ while comparison < num_comparisons:
 												length_unit='nm' )
 		simulation.run()
 
-		# go.show_near_field(
-		# 			quantities_to_plot=['norm(E)'],
-		# 			show_plots=False,
-		# 			show_opts=[{'label':'raw_data'},
-		# 						{'interpolation':'quadric'},
-		# 						{'interpolation':'quadric'}],
-		# 			save_plots=True,
-		# 			save_opts=[{'format':'png'},
-		# 						{'format':'pdf','dpi':200},
-		# 						{'format':'gif'}], # animated gif of E_y
-		# 			outputdir=(projects_directory_location + "/smuthi_output_" + str( comparison + job_idx ) + "/"),
-		# 			xmin=-1000,
-		# 			xmax=1000,
-		# 			ymin=-1000,
-		# 			ymax=1000,
-		# 			zmin=focal_length_nm,
-		# 			zmax=focal_length_nm,
-		# 			resolution_step=20,
-		# 			simulation=simulation,
-		# 			show_internal_field=False)
+		go.show_near_field(
+					quantities_to_plot=['norm(E)'],
+					show_plots=False,
+					show_opts=[{'label':'raw_data'},
+								{'interpolation':'quadric'},
+								{'interpolation':'quadric'}],
+					save_plots=True,
+					save_opts=[{'format':'png'},
+								{'format':'pdf','dpi':200},
+								{'format':'gif'}], # animated gif of E_y
+					outputdir=(projects_directory_location + "/smuthi_output_" + str( comparison + job_idx ) + "/"),
+					xmin=-1000,
+					xmax=1000,
+					ymin=-1000,
+					ymax=1000,
+					zmin=focal_length_nm,
+					zmax=focal_length_nm,
+					resolution_step=20,
+					simulation=simulation,
+					show_internal_field=False)
 
-"""
-		mie_cluster = miepy.sphere_cluster(
-			medium=background_dielectric,
-			position=random_centers, radius=random_radii, material=random_materials,
-			source=plane_wave, wavelength=(probe_wavelength_nm*nm), lmax=lmax, interface=air_interface )
 
-		gmmt_data[ comparison + job_idx, 0 ] = np.sum(
-			np.abs( mie_cluster.E_field( 0.25 * device_size_x_nm * nm, 0.25 * device_size_y_nm * nm, focal_z_nm * nm ) )**2 )
-		gmmt_data[ comparison + job_idx, 1 ] = np.sum(
-			np.abs( mie_cluster.E_field( -0.25 * device_size_x_nm * nm, 0.25 * device_size_y_nm * nm, focal_z_nm * nm ) )**2 )
-		gmmt_data[ comparison + job_idx, 2 ] = np.sum(
-			np.abs( mie_cluster.E_field( -0.25 * device_size_x_nm * nm, -0.25 * device_size_y_nm * nm, focal_z_nm * nm ) )**2 )
-		gmmt_data[ comparison + job_idx, 3 ] = np.sum(
-			np.abs( mie_cluster.E_field( 0.25 * device_size_x_nm * nm, -0.25 * device_size_y_nm * nm, focal_z_nm * nm ) )**2 )
+		# mie_cluster = miepy.sphere_cluster(
+		# 	medium=background_dielectric,
+		# 	position=random_centers, radius=random_radii, material=random_materials,
+		# 	source=plane_wave, wavelength=(probe_wavelength_nm*nm), lmax=lmax, interface=air_interface )
 
-		gmmt_focal_intensity[ comparison + job_idx ] = np.sum(
-			np.abs( mie_cluster.E_field( focal_lateral_search_mesh_x_nm, focal_lateral_search_mesh_y_nm, focal_lateral_search_mesh_z_nm ) )**2,
-			axis=0 )
-"""
+		# gmmt_data[ comparison + job_idx, 0 ] = np.sum(
+		# 	np.abs( mie_cluster.E_field( 0.25 * device_size_x_nm * nm, 0.25 * device_size_y_nm * nm, focal_z_nm * nm ) )**2 )
+		# gmmt_data[ comparison + job_idx, 1 ] = np.sum(
+		# 	np.abs( mie_cluster.E_field( -0.25 * device_size_x_nm * nm, 0.25 * device_size_y_nm * nm, focal_z_nm * nm ) )**2 )
+		# gmmt_data[ comparison + job_idx, 2 ] = np.sum(
+		# 	np.abs( mie_cluster.E_field( -0.25 * device_size_x_nm * nm, -0.25 * device_size_y_nm * nm, focal_z_nm * nm ) )**2 )
+		# gmmt_data[ comparison + job_idx, 3 ] = np.sum(
+		# 	np.abs( mie_cluster.E_field( 0.25 * device_size_x_nm * nm, -0.25 * device_size_y_nm * nm, focal_z_nm * nm ) )**2 )
+
+		# gmmt_focal_intensity[ comparison + job_idx ] = np.sum(
+		# 	np.abs( mie_cluster.E_field( focal_lateral_search_mesh_x_nm, focal_lateral_search_mesh_y_nm, focal_lateral_search_mesh_z_nm ) )**2,
+		# 	axis=0 )
+
 
 		# for x_idx in range( 0, focal_x_points ):
 		# 	for y_idx in range( 0, focal_y_points ):
