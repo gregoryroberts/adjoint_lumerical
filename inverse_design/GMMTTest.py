82a08b376757ab9a1d2a1e0c434eb5c94a1e0c86
Greg <gdrobert@caltech.edu>
2020-08-08 14:07:08 -0700

    test

--- inverse_design/GMMTTest.py
+++ inverse_design/GMMTTest.py
@@ -235,8 +235,8 @@ while comparison < num_comparisons:
 												particle_list=smuthi_spheres,
 												initial_field=smuthi_plane_wave,
 												solver_type='gmres',
-												store_coupling_matrix=True,
-												coupling_matrix_interpolator_kind='linear',
+												store_coupling_matrix=False,
+												coupling_matrix_interpolator_kind='cubic',
 												coupling_matrix_lookup_resolution=5,#5,
 												solver_tolerance=1e-4,
 												length_unit='nm' )
