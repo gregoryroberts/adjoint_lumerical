ecfd1651a5d9095846c8eba8db65cbc576086924
Greg <gdrobert@caltech.edu>
2020-08-19 16:57:18 -0700

    another attempt

--- inverse_design/Landscape/ColorSplittingOptimization2D.py
+++ inverse_design/Landscape/ColorSplittingOptimization2D.py
@@ -518,7 +518,8 @@ class ColorSplittingOptimization2D():
 				]
 
 				# local_gradient_device = fom_scaling * 2 * omega * eps_nought * np.real( make_current * bare_local_adj_Ez / 1j )
-				local_gradient_device = fom_scaling * 2 * ( 1. / omega ) * np.real( make_current * bare_local_adj_Ez / 1j )
+				# local_gradient_device = fom_scaling * 2 * ( 1. / omega ) * np.real( make_current * bare_local_adj_Ez / 1j )
+				local_gradient_device = fom_scaling * 2 * ( 1. / omega ) * np.real( make_current * local_adj_Ez / 1j )
 
 				# local_gradient_device = fom_scaling * 2 * omega * eps_nought * np.real( local_p_ind * local_adj_Ez / 1j )
 				# local_gradient_device = fom_scaling * 2 * omega * eps_nought * np.real( local_p_ind * bare_local_adj_Ez / 1j )
