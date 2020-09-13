95be0462e726fa6b44804c1d2dfb16fb44c412ac
Greg <gdrobert@caltech.edu>
2020-09-12 22:44:12 -0700

    level set

---
+++ inverse_design/LevelSetGlobalOptimize2DParameters.py
@@ -0,0 +1,164 @@
+#
+# Parameter file for the Bayer Filter Layered Lithography optimization
+#
+
+import numpy as np
+import sys
+
+import FreeOptimizationMultiDevice
+import OptimizationLayersSpacersMultiDevice
+import OptimizationLayersSpacersGlobalBinarization2DMultiDevice
+
+# import FreeBayerFilterWithBlur2D
+# import FreeBayerFilterDualPhaseBlur2D
+
+#
+# Files
+#
+project_name = 'cmos_dielectric_2d_3focal_layered_higherindex_p22layers_rbg_lsf_global_3xtsmc_um_focal_1p5um'
+
+#
+# Optical
+#
+# todo: the side background index shouldn't be TiO2 because that part will not be etched away!  Go back to oxide!
+# Maybe force binarize more slowly.  Consider just optimizing for Strell ratio if we aren't going to get high transmisison anyway.
+# Check binarized device and look at feature sizes!
+background_index = 1.0
+design_index_background = 1.0#1.35
+device_background_index = 1.0#1.35# 2.5
+high_index_backfill = 2.5
+substrate_index = 1.0
+
+min_real_permittivity = design_index_background**2
+max_real_permittivity = high_index_backfill**2
+
+min_imag_permittivity = 0
+max_imag_permittivity = 0
+
+min_real_index = np.sqrt( min_real_permittivity )
+max_real_index = np.sqrt( max_real_permittivity )
+
+permittivity_bounds = np.array( [ min_real_permittivity + 1j * min_imag_permittivity, max_real_permittivity + 1j * max_imag_permittivity ], dtype=np.complex )
+
+init_permittivity_0_1_scale = 0.5
+
+lsf_start_step_size = 0.1
+lsf_end_step_size = 0.05
+
+# For the focal length, should we put these things in a near field regime.  In the nature photonics, with very
+# simple scatterers made of SiN, they can get fairly impressive splitting.  The "focal length" seems to be smaller
+# there (not sure if it would be considered near field or not, but it would be right on the edge).
+focal_length_um = 1.5
+
+
+#
+# Device
+#
+mesh_spacing_um = 0.025
+lsf_mesh_spacing_um = 0.005
+
+device_layer_thicknesses_um = np.array( [
+	0.22, 0.22, 0.22, 0.22, 0.22, 0.22, 0.13
+] )
+device_spacer_thicknesses_um = np.array( [
+	0.175, 0.175, 0.175, 0.175, 0.175, 0.175, 0.0
+] )
+
+feature_size_voxels_by_profiles = [ int( 0.1 / lsf_mesh_spacing_um ) for idx in range( 0, 6 ) ]
+feature_size_voxels_by_profiles.append( int( 0.09 / lsf_mesh_spacing_um ) )
+
+
+device_size_lateral_um = 3.0
+designable_size_vertical_um = np.sum( device_layer_thicknesses_um ) + np.sum( device_spacer_thicknesses_um )
+
+device_size_verical_um = designable_size_vertical_um
+
+opt_device_voxels_lateral = 1 + int( device_size_lateral_um / lsf_mesh_spacing_um )
+device_voxels_lateral = 1 + int(device_size_lateral_um / mesh_spacing_um)
+designable_device_voxels_vertical = 1 + int(designable_size_vertical_um / mesh_spacing_um)
+
+designable_device_vertical_maximum_um = designable_size_vertical_um
+designable_device_vertical_minimum_um = 0
+
+#
+# Spectral
+#
+num_bands = 3
+num_points_per_band = 10#15
+
+lambda_min_um = 0.45
+lambda_max_um = 0.65
+
+num_design_frequency_points = num_bands * num_points_per_band
+num_wavelengths = num_design_frequency_points
+num_eval_frequency_points = 1 * num_design_frequency_points
+
+lambda_values_um = np.linspace(lambda_min_um, lambda_max_um, num_design_frequency_points)
+max_intensity_by_wavelength = (device_size_lateral_um * 1.02)**2 / (focal_length_um**2 * lambda_values_um**2)
+
+
+
+#
+# FDTD
+#
+vertical_gap_size_um = 1.0
+lateral_gap_size_um = 1.0
+
+fdtd_region_size_vertical_um = 2 * vertical_gap_size_um + device_size_verical_um + focal_length_um
+fdtd_region_size_lateral_um = 2 * lateral_gap_size_um + device_size_lateral_um
+fdtd_region_maximum_vertical_um = device_size_verical_um + vertical_gap_size_um
+fdtd_region_minimum_vertical_um = -vertical_gap_size_um - focal_length_um
+
+fdtd_region_minimum_vertical_voxels = int( np.ceil(fdtd_region_size_vertical_um / mesh_spacing_um) )
+fdtd_region_minimum_lateral_voxels = int( np.ceil(fdtd_region_size_lateral_um / mesh_spacing_um) )
+
+fdtd_region_size_lateral_voxels = int( np.ceil( fdtd_region_size_lateral_um / mesh_spacing_um ) )
+
+
+fdtd_simulation_time_fs = 3 * 2000
+
+#
+# Forward Source
+#
+num_polarizations = 2
+
+lateral_aperture_um = 1.1 * device_size_lateral_um
+src_maximum_vertical_um = device_size_verical_um + 0.5 * vertical_gap_size_um
+src_minimum_vertical_um = -focal_length_um - 0.5 * vertical_gap_size_um
+
+#
+# Spectral and polarization selectivity information
+#
+polarizations_focal_plane_map = [ ['x', 'y'], ['x', 'y'], ['x', 'y'], ['x', 'y'] ]
+weight_focal_plane_map = [ 1.0, 1.0, 1.0, 1.0 ]
+polarization_name_to_idx = { 'x':0, 'y':1, 'z':2 }
+# We are assuming that the data is organized in order of increasing wavelength (i.e. - blue first, red last)
+spectral_focal_plane_map = [
+	[2 * num_points_per_band, 3 * num_points_per_band],
+	[0, num_points_per_band],
+	[num_points_per_band, 2 * num_points_per_band],
+]
+
+# spectral_focal_plane_map = [
+# 	[0 * num_points_per_band, 2 * num_points_per_band],
+# 	[0, 3 * num_points_per_band],
+# 	[num_points_per_band, 3 * num_points_per_band],
+# ]
+#
+# Adjoint sources
+#
+# This seems like a long focal length
+adjoint_vertical_um = -focal_length_um
+num_focal_spots = 3
+num_adjoint_sources = num_focal_spots
+
+# adjoint_x_positions_um = [ -3 * device_size_lateral_um / 8., -device_size_lateral_um / 8., device_size_lateral_um / 8., 3 * device_size_lateral_um / 8. ]
+adjoint_x_positions_um = [ -device_size_lateral_um / 3., 0.0, device_size_lateral_um / 3. ]
+
+#
+# Optimization
+#
+num_epochs = 1
+num_iterations_per_epoch = 100#80
+init_optimization_epoch = 0
+
---
+++ inverse_design/level_set_cmos.py
@@ -0,0 +1,207 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from LevelSet import LevelSet
+import OptimizationState
+
+
+class LevelSetCMOS( OptimizationState.OptimizationState ):
+
+	def __init__(
+		self,
+		permittivity_bounds,
+		opt_mesh_size_um, opt_vertical_start_um, opt_width_um,
+		minimum_feature_gap_spacing_voxels, layer_thicknesses_um, layer_spacings_um,
+		num_iterations, num_epochs, filename_prefix ):
+
+		super( LevelSetCMOS, self ).__init__( num_iterations, num_epochs, filename_prefix )
+
+		self.permittivity_bounds = permittivity_bounds
+		self.opt_mesh_size_um = opt_mesh_size_um
+
+		self.layer_thicknesses_um = layer_thicknesses_um
+		self.layer_spacings_um = layer_spacings_um
+
+		self.num_devices = 1
+
+		self.minimum_feature_gap_spacing_voxels = minimum_feature_gap_spacing_voxels
+
+		self.layer_thicknesses_voxels = np.array( [ int( thickness_um / self.opt_mesh_size_um ) for thickness_um in self.layer_thicknesses_um ] )
+		self.spacer_thicknesses_voxels = np.array( [ int( spacing_um / self.opt_mesh_size_um ) for spacing_um in self.layer_spacings_um ] )
+
+		self.opt_width_um = opt_width_um
+
+		self.opt_vertical_start_um = opt_vertical_start_um
+		self.opt_vertical_end_um = self.opt_vertical_start_um + np.sum( self.layer_thicknesses_um + self.layer_spacings_um )
+
+		self.opt_vertical_num_voxels = int( ( self.opt_vertical_end_um - self.opt_vertical_start_um ) / self.opt_mesh_size_um )
+		self.opt_width_num_voxels = int( self.opt_width_um / self.opt_mesh_size_um )
+
+		self.layer_profiles = [ np.zeros( self.opt_width_num_voxels ) for idx in range( 0, len( self.layer_thicknesses_um ) ) ]
+
+		self.level_sets = [ None for idx in range( 0, len( self.layer_thicknesses_um ) ) ]
+		self.level_set_boundary_smoothing = 1
+
+
+	def randomize_layer_profiles( self, feature_gap_width_sigma_voxels, feature_probability ):
+		for profile_idx in range( 0, len( self.layer_profiles ) ):
+			space_remaining_voxels = self.opt_width_num_voxels
+
+			self.layer_profiles[ profile_idx][ : ] = 1.0
+
+			while space_remaining_voxels >= self.minimum_feature_gap_spacing_voxels[ profile_idx ]:
+				choose_density_value = 1.0 * ( np.random.uniform( 0, 1 ) < feature_probability )
+				choose_width_voxels = int(
+					np.minimum( space_remaining_voxels,
+						self.minimum_feature_gap_spacing_voxels[ profile_idx ] + np.maximum( 0, np.random.normal( 0, feature_gap_width_sigma_voxels, 1 ) ) )
+				)
+
+				end_pt = space_remaining_voxels
+				start_pt = np.maximum( end_pt - choose_width_voxels, 0 )
+
+				space_remaining_voxels -= choose_width_voxels
+
+				self.layer_profiles[ profile_idx ][ start_pt : end_pt ] = choose_density_value
+
+	def assemble_level_sets( self ):
+		for profile_idx in range( 0, len( self.layer_profiles ) ):
+			layer_density = np.zeros( ( self.opt_width_num_voxels, 3 ) )
+
+			for internal_idx in range( 0, 3 ):
+				layer_density[ :, internal_idx ] = self.layer_profiles[ profile_idx ]
+
+			self.level_sets[ profile_idx ] = LevelSet.LevelSet( layer_density.shape, self.level_set_boundary_smoothing )
+			self.level_sets[ profile_idx ].init_with_density( layer_density )
+
+
+	def assemble_index( self, device_idx=1 ):
+		return np.sqrt(
+					self.permittivity_bounds[ 0 ] +
+					( self.permittivity_bounds[ 1 ] - self.permittivity_bounds[ 0 ] ) * self.assemble_density() )
+
+	def assemble_density( self ):
+		device_density = np.zeros( ( self.opt_width_num_voxels, self.opt_vertical_num_voxels ) )
+
+		for profile_idx in range( 0, len( self.layer_profiles ) ):
+			get_start = np.sum( self.layer_thicknesses_voxels[ 0 : profile_idx ] ) + np.sum( self.spacer_thicknesses_voxels[ 0 : profile_idx ] )
+			get_end = get_start + self.layer_thicknesses_voxels[ profile_idx ]
+
+			get_profile = self.level_sets[ profile_idx ].device_density_from_level_set()
+			get_profile = get_profile[ :, 1 ]
+
+			for internal_idx in range( 0, self.layer_thicknesses_voxels[ profile_idx ] ):
+				device_density[ :, get_start + internal_idx ] = get_profile#self.layer_profiles[ profile_idx ]
+
+		return device_density
+
+	def update( self, gradient_real, graident_imag, gradient_real_lsf, gradient_imag_lsf, epoch, iteration ):
+
+		gradient_real_interpolate = self.reinterpolate( np.squeeze( gradient_real ), [ self.opt_width_num_voxels, self.opt_vertical_num_voxels ] )
+
+		max_abs_movement = 0
+		for profile_idx in range( 0, len( self.layer_profiles ) ):
+			get_start = np.sum( self.layer_thicknesses_voxels[ 0 : profile_idx ] ) + np.sum( self.spacer_thicknesses_voxels[ 0 : profile_idx ] )
+			get_end = get_start + self.layer_thicknesses_voxels[ profile_idx ]
+
+			average_velocity = np.mean( -gradient_real_interpolate[ :, get_start : get_end ], axis=1 )
+
+			max_abs_movement = np.maximum( max_abs_movement, np.max( np.abs( average_velocity ) ) )
+
+
+		for profile_idx in range( 0, len( self.layer_profiles ) ):
+			get_start = np.sum( self.layer_thicknesses_voxels[ 0 : profile_idx ] ) + np.sum( self.spacer_thicknesses_voxels[ 0 : profile_idx ] )
+			get_end = get_start + self.layer_thicknesses_voxels[ profile_idx ]
+
+			average_velocity = np.mean( -gradient_real_interpolate[ :, get_start : get_end ], axis=1 ) / max_abs_movement
+			expand_velocity = np.zeros( ( self.opt_width_num_voxels, 3 ) )
+
+			for internal_idx in range( 0, 3 ):
+				expand_velocity[ :, internal_idx ] = average_velocity
+
+			get_lsf_layer = self.level_sets[ profile_idx ]
+			get_lsf_layer.update( expand_velocity, 5 )
+
+		#
+		# After each update, we need to go through and make sure nothing violates the feature size.
+		# We may have feature/gap size by layer at some point (i.e. - M1 can have 90nm while the rest have 100nm)
+		#
+
+		#
+		# We will pick a random starting point each time through the profile and fix it up that way.  This way we aren't
+		# always undoing the same changes
+		#
+
+		for profile_idx in range( 0, len( self.layer_profiles ) ):
+			get_profile = self.level_sets[ profile_idx ].device_density_from_level_set()
+			get_profile = np.squeeze( get_profile[ :, 1 ] )
+
+			flip = np.random.random( 1 )[ 0 ] > 0.5
+
+			if flip:
+				get_profile = np.flip( get_profile )
+
+			cur_loc = int( np.random.random( 1 )[ 0 ] * self.opt_width_num_voxels )
+			start_value = get_profile[ cur_loc ] > 0.5
+
+			while ( cur_loc > 0 ):
+				cur_loc -= 1
+
+				cur_value = get_profile[ cur_loc ] > 0.5
+
+				if cur_value != start_value:
+					cur_loc += 1
+					break
+
+			num_voxels_scanned = 0
+			while ( num_voxels_scanned < self.opt_width_num_voxels ):
+
+				num_voxels_feature_gap = 0
+
+				cur_value = get_profile[ cur_loc ] > 0.5
+				start_loc = cur_loc
+				hit_end = False
+
+				while ( cur_value == start_value ) and ( num_voxels_feature_gap < self.opt_width_num_voxels ):
+					num_voxels_feature_gap += 1
+
+					cur_loc += 1
+
+					if cur_loc == self.opt_width_num_voxels:
+						hit_end = True
+						cur_loc = 0
+						break
+
+					cur_value = get_profile[ cur_loc ] > 0.5
+
+					num_voxels_scanned += 1
+
+
+				if ( num_voxels_feature_gap < self.minimum_feature_gap_spacing_voxels[ profile_idx ] ):
+					if ( start_loc > 0 ) and ( not hit_end ):
+						while ( num_voxels_feature_gap < self.minimum_feature_gap_spacing_voxels[ profile_idx ] ) and ( cur_loc < self.opt_width_num_voxels ):
+							get_profile[ cur_loc ] = 1.0 * start_value
+
+							cur_loc += 1
+							num_voxels_feature_gap += 1
+							num_voxels_scanned += 1
+
+				cur_loc = ( cur_loc % self.opt_width_num_voxels )
+				start_value = get_profile[ cur_loc ] > 0.5
+
+			if flip:
+				get_profile = np.flip( get_profile )
+
+
+			new_layer_density = np.zeros( ( self.opt_width_num_voxels, 3 ) )
+
+			for internal_idx in range( 0, 3 ):
+				new_layer_density[ :, internal_idx ] = get_profile
+
+			self.level_sets[ profile_idx ].init_with_density( new_layer_density )
+
+	def save_design( self, filebase, epoch ):
+		return
+
+
+
+
+
