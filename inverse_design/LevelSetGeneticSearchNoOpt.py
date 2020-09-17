a9c1d08539c3de3747601ac1893157ba4ec33652
Greg <gdrobert@caltech.edu>
2020-09-17 07:48:49 -0700

    green restart

--- inverse_design/LevelSetGeneticSearchNoOpt.py
+++ inverse_design/LevelSetGeneticSearchNoOpt.py
@@ -567,8 +567,10 @@ for generation_idx in range( 0, num_generations ):
 
 				generation_fom_by_wl[ individual_idx ] += pol_weights[ pol_idx ] * figure_of_merit_total
 
-
 		generation_fom[ individual_idx ] = figure_of_merit_by_device[ 0 ]
+
+		if np.isnan( figure_of_merit_by_device[ 0 ] ):
+			np.save( projects_directory_location + '/nan_device_' + str( generation_idx ) + "_" + str( individual_idx ) + ".npy", get_index )
 
 	search_fom.append( generation_fom )
 	search_devices.append( generation_devices )
