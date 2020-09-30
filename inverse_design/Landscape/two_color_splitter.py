ec4adf1fc72d25f493ad6104780f64def0ec87fe
Greg <gdrobert@caltech.edu>
2020-09-30 14:32:45 -0700

    design

--- inverse_design/Landscape/two_color_splitter.py
+++ inverse_design/Landscape/two_color_splitter.py
@@ -99,7 +99,7 @@ for idx in range( int( 0.5 * num_dense_wls ), num_dense_wls ):
 
 binarize = True
 binarize_movement_per_step_nominal = 0.0075
-binarize_max_movement_per_voxel_nominal = 0.0075 / 10.
+binarize_max_movement_per_voxel_nominal = 0.0075# / 10.
 
 rho_delta_scaling = ( 1.5**2 - np.real( min_relative_permittivity ) ) / np.real( max_relative_permittivity - min_relative_permittivity )
 binarize_movement_per_step = binarize_movement_per_step_nominal * rho_delta_scaling
