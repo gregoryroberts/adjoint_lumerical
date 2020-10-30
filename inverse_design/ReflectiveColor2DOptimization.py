ee797c7aa27ec12255b563d198f31dc307327af8
Greg <gdrobert@caltech.edu>
2020-10-29 18:39:31 -0700

    add silicong

--- inverse_design/ReflectiveColor2DOptimization.py
+++ inverse_design/ReflectiveColor2DOptimization.py
@@ -133,7 +133,7 @@ if not os.path.isdir(projects_directory_location):
 
 should_reload = False
 # projects_directory_reload = projects_directory_location + "/" + project_name + "_continuous_Hz_sio2_no_constrast_v2"
-projects_directory_location += "/" + project_name + "_continuous_reflective_red_tio2_v23"
+projects_directory_location += "/" + project_name + "_continuous_reflective_red_tio2_v24"
 
 if not os.path.isdir(projects_directory_location):
 	os.mkdir(projects_directory_location)
@@ -504,13 +504,13 @@ elongations_right_um = [ 0, 1 ]
 # 	side_blocks.append( side_block )
 
 
-# bottom_silicon = fdtd_hook.addrect()
-# bottom_silicon['name'] = 'bottom_silicon'
-# bottom_silicon['y min'] = fdtd_region_minimum_vertical_um * 1e-6
-# bottom_silicon['y max'] = designable_device_vertical_minimum_um * 1e-6
-# bottom_silicon['x span'] = 1.5 * fdtd_region_size_lateral_um * 1e-6
-# bottom_silicon['material'] = 'Si (Silicon) - Palik'
-# fdtd_hook.addtogroup( device_and_backgrond_group['name'] )
+bottom_silicon = fdtd_hook.addrect()
+bottom_silicon['name'] = 'bottom_silicon'
+bottom_silicon['y min'] = fdtd_region_minimum_vertical_um * 1e-6
+bottom_silicon['y max'] = designable_device_vertical_minimum_um * 1e-6
+bottom_silicon['x span'] = 1.5 * fdtd_region_size_lateral_um * 1e-6
+bottom_silicon['material'] = 'Si (Silicon) - Palik'
+fdtd_hook.addtogroup( device_and_backgrond_group['name'] )
 
 
 gaussian_normalization = np.zeros( num_points_per_band )
@@ -1663,7 +1663,7 @@ fdtd_hook.set('enabled', 1)
 
 # check_gradient_full( 1 )
 
-# load_index = np.load('/Users/gregory/Downloads/device_final_redirect_si_10p8_red_tio2_v21.npy')
+# load_index = np.load('/Users/gregory/Downloads/device_1_redirect_si_10p8_red_tio2_v23.npy')
 # plt.imshow( load_index )
 # plt.colorbar()
 # plt.show()
