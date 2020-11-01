a963870fd4fdc686dd976415d6c6c2a0eceabae2
Greg <gdrobert@caltech.edu>
2020-10-31 23:08:43 -0700

    filepath lumapi

--- inverse_design/ReflectiveColor2DOptimization.py
+++ inverse_design/ReflectiveColor2DOptimization.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
 
 from ReflectiveColor2DParameters import *
 
-run_on_cluster = False#True
+run_on_cluster = True
 
 if run_on_cluster:
 	import imp
