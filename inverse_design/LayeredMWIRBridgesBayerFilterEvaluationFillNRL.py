e0cd8a878c0f367aea2703711148205c9018c5d0
Greg <gdrobert@caltech.edu>
2021-01-05 07:28:07 -0800

    double dilation

--- inverse_design/LayeredMWIRBridgesBayerFilterEvaluationFillNRL.py
+++ inverse_design/LayeredMWIRBridgesBayerFilterEvaluationFillNRL.py
@@ -83,7 +83,7 @@ python_src_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '
 
 projects_directory_location_base = "/central/groups/Faraon_Computing/projects" 
 projects_directory_location_base += "/" + project_name
-projects_directory_location = projects_directory_location_base + '_filled_eroded'
+projects_directory_location = projects_directory_location_base + '_filled_dilated_double'
 
 
 if not os.path.isdir(projects_directory_location):
@@ -310,7 +310,7 @@ bayer_filter = LayeredMWIRBridgesBayerFilterNRL.LayeredMWIRBridgesBayerFilterNRL
 	topology_num_free_iterations_between_patches)
 
 dilation_erosion_test = True
-dilation_amount = -1
+dilation_amount = 2
 
 bayer_filter_region_x = 1e-6 * np.linspace(-0.5 * device_size_lateral_um, 0.5 * device_size_lateral_um, device_voxels_lateral)
 bayer_filter_region_y = 1e-6 * np.linspace(-0.5 * device_size_lateral_um, 0.5 * device_size_lateral_um, device_voxels_lateral)
