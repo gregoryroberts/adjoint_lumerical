a274131eac57f5d11f89ffc1b57808c0f0cd578a
Greg <gdrobert@caltech.edu>
2021-01-06 15:52:45 -0800

    rerun binarize v2

--- inverse_design/Landscape/run_binarize.py
+++ inverse_design/Landscape/run_binarize.py
@@ -127,8 +127,8 @@ blur_fields = False#True
 # num_iterations_nominal = 150
 # num_iterations_nominal = 300
 # num_iterations_nominal = 300#360
-# num_iterations_nominal = 320
-num_iterations_nominal = 80
+num_iterations_nominal = 320
+# num_iterations_nominal = 80
 # num_iterations_nominal = 200
 num_iterations = int( np.ceil(
 	num_iterations_nominal * ( max_relative_permittivity - min_relative_permittivity ) / ( 1.5**2 - min_relative_permittivity ) ) )
