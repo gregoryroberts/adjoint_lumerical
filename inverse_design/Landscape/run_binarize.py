16c0c76ea935bc9bd185cf70cc6fed85bfb168dc
Greg <gdrobert@caltech.edu>
2021-01-06 15:53:47 -0800

    restart again

--- inverse_design/Landscape/run_binarize.py
+++ inverse_design/Landscape/run_binarize.py
@@ -127,8 +127,8 @@ blur_fields = False#True
 # num_iterations_nominal = 150
 # num_iterations_nominal = 300
 # num_iterations_nominal = 300#360
-num_iterations_nominal = 320
-# num_iterations_nominal = 80
+# num_iterations_nominal = 320
+num_iterations_nominal = 80
 # num_iterations_nominal = 200
 num_iterations = int( np.ceil(
 	num_iterations_nominal * ( max_relative_permittivity - min_relative_permittivity ) / ( 1.5**2 - min_relative_permittivity ) ) )
@@ -432,7 +432,7 @@ else:
 			# 	index_regularization,
 			# 	downsample_abs_max )
 
-			binarize_v2 = True#False#True
+			binarize_v2 = False#True
 			eps_movement_per_voxel = 0.02
 			if binarize_v2:
 				binarize_max_movement_per_voxel = eps_movement_per_voxel / ( max_relative_permittivity - min_relative_permittivity )
@@ -451,7 +451,7 @@ else:
 					downsample_abs_max, binarize_v2, 0.1 )
 			else:
 				old_density = np.load(
-					'/central/groups/Faraon_Computing/projects/binarize_bin_rate_down_avg_wider_save_v9_' +
+					'/central/groups/Faraon_Computing/projects/binarize_bin_rate_down_avg_wider_save_v12_' +
 					index_to_name[ max_index ] +
 					'/opt_optimized_density.npy' )
 
