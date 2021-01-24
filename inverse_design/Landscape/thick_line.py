315b6dee726bdf2a5f1ad187ea5d071668de5796
Greg <gdrobert@caltech.edu>
2021-01-24 08:55:50 -0800

    thick line

--- inverse_design/Landscape/thick_line.py
+++ inverse_design/Landscape/thick_line.py
@@ -112,7 +112,7 @@ make_optimizer = ColorSplittingOptimization2D.ColorSplittingOptimization2D(
 
 
 loc0 = '/central/groups/Faraon_Computing/projects/ten_um_bin_sum_fom_ratio_wide_longer_f_v1_1p5/'
-loc1 = '/central/groups/Faraon_Computing/projects/ten_um_bin_sum_fom_ratio_wide_longer_f_v2_1p5/'
+loc1 = '/central/groups/Faraon_Computing/projects/ten_um_bin_sum_fom_ratio_wide_longer_f_v3_1p5/'
 
 density0 = np.load( loc0 + 'opt_optimized_density.npy' )
 density1 = np.load( loc1 + 'opt_optimized_density.npy' )
@@ -131,8 +131,8 @@ for alpha_idx in range( 0, num_alpha ):
 
 	fom_line[ alpha_idx ] = net_fom
 
-np.save( '/central/groups/Faraon_Computing/projects/ten_um_bin_sum_fom_ratio_wide_longer_f_v1_1p5/fom_line_v1_v2.npy', fom_line )
-np.save( '/central/groups/Faraon_Computing/projects/ten_um_bin_sum_fom_ratio_wide_longer_f_v2_1p5/fom_line_v1_v2.npy', fom_line )
+np.save( '/central/groups/Faraon_Computing/projects/ten_um_bin_sum_fom_ratio_wide_longer_f_v1_1p5/fom_line_v1_v3.npy', fom_line )
+np.save( '/central/groups/Faraon_Computing/projects/ten_um_bin_sum_fom_ratio_wide_longer_f_v2_1p5/fom_line_v1_v3.npy', fom_line )
 
 
 
